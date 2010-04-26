9207dbb9d882f8fa2a880a9840f5283df6a52c7e
jub <jub@parostroj.net>
2010-04-26 15:53:19 +0200

    [gui] text item events affect status of the model (changed)

--- grafikon-gui/src/main/java/net/parostroj/timetable/gui/ApplicationModelColleague.java
+++ grafikon-gui/src/main/java/net/parostroj/timetable/gui/ApplicationModelColleague.java
@@ -1,6 +1,9 @@
 package net.parostroj.timetable.gui;
 
 import net.parostroj.timetable.mediator.AbstractColleague;
+import net.parostroj.timetable.model.events.GTEventType;
+import net.parostroj.timetable.model.events.TextItemEvent;
+import net.parostroj.timetable.model.events.TrainDiagramEvent;
 
 /**
  * Colleague for application model.
@@ -9,13 +12,24 @@ import net.parostroj.timetable.mediator.AbstractColleague;
  */
 public class ApplicationModelColleague extends AbstractColleague implements ApplicationModelListener {
 
+    private ApplicationModel model;
+
     public ApplicationModelColleague(ApplicationModel model) {
+        this.model = model;
         model.addListener(this);
     }
 
     @Override
     public void receiveMessage(Object message) {
-        // do not react to messages
+        // react to some events to set changed model
+        if (message instanceof TrainDiagramEvent) {
+            TrainDiagramEvent tde = (TrainDiagramEvent)message;
+            if (tde.getType() == GTEventType.TEXT_ITEM_ADDED || tde.getType() == GTEventType.TEXT_ITEM_REMOVED ||
+                    tde.getType() == GTEventType.TEXT_ITEM_MOVED)
+                model.setModelChanged(true);
+            if (tde.getType() == GTEventType.NESTED && tde.getLastNestedEvent() instanceof TextItemEvent)
+                model.setModelChanged(true);
+        }
     }
 
     @Override
