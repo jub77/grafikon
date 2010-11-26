582b96fedee8d6b5ffa5b6753c8cd9bc08f1c00c
jub <jub@parostroj.net>
2010-11-26 17:40:04 +0100

    [model] precompiled mvel templates

--- grafikon-model/src/main/java/net/parostroj/timetable/model/TextTemplateMvel.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/TextTemplateMvel.java
@@ -1,6 +1,9 @@
 package net.parostroj.timetable.model;
 
 import java.util.Map;
+
+import org.mvel2.templates.CompiledTemplate;
+import org.mvel2.templates.TemplateCompiler;
 import org.mvel2.templates.TemplateRuntime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,15 +16,19 @@ import org.slf4j.LoggerFactory;
 public final class TextTemplateMvel extends TextTemplate {
     
     private static final Logger LOG = LoggerFactory.getLogger(TextTemplateMvel.class);
+    
+    private CompiledTemplate compiledTemplate;
 
     protected TextTemplateMvel(String template) {
         super(template);
+        TemplateCompiler compiler = new TemplateCompiler(template);
+        compiledTemplate = compiler.compile();
     }
 
     @Override
     public String evaluateWithException(Map<String, Object> binding) throws GrafikonException {
         try {
-            return (String)TemplateRuntime.eval(getTemplate(), binding);
+            return (String)TemplateRuntime.execute(compiledTemplate, binding);
         } catch (Exception e) {
             throw new GrafikonException("Error evaluating template: " + e.getMessage(), e, GrafikonException.Type.TEXT_TEMPLATE);
         }
