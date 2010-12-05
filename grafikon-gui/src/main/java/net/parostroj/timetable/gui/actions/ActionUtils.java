048b7805f74eb029e80fe5d8d7ca68f2c2ef9649
jub <jub@parostroj.net>
2010-12-05 10:39:09 +0100

    [gui] removed helper method for running in EDT

--- grafikon-gui/src/main/java/net/parostroj/timetable/gui/actions/ActionUtils.java
+++ grafikon-gui/src/main/java/net/parostroj/timetable/gui/actions/ActionUtils.java
@@ -5,12 +5,8 @@ import java.awt.Window;
 
 import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
-import javax.swing.SwingUtilities;
 
 import net.parostroj.timetable.utils.ResourceLoader;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Utility class.
@@ -19,8 +15,6 @@ import org.slf4j.LoggerFactory;
  */
 public class ActionUtils {
     
-    private static final Logger LOG = LoggerFactory.getLogger(ActionUtils.class);
-
     public static Component getTopLevelComponent(Object component) {
         if (component == null || !(component instanceof Component)) {
             return null;
@@ -46,16 +40,5 @@ public class ActionUtils {
 
     public static void showWarning(String text, Component parent) {
         JOptionPane.showMessageDialog(parent, text, ResourceLoader.getString("dialog.warning.title"), JOptionPane.WARNING_MESSAGE);
-    }
-    
-    public static void runInEDT(Runnable runnable) {
-        if (SwingUtilities.isEventDispatchThread())
-            runnable.run();
-        else
-            try {
-                SwingUtilities.invokeAndWait(runnable);
-            } catch (Exception e) {
-                LOG.error("Error execution action in dispatch thread.", e);
-            }
     }
 }
