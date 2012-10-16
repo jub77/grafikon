f957c7a679b3a2641621be57f34acc13ba22213c
jub <jub@parostroj.net>
2012-10-16 12:15:48 +0200

    [model] added group removal utility

---
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/TrainAttributes.java
@@ -0,0 +1,11 @@
+package net.parostroj.timetable.model;
+
+/**
+ * Attributes of train.
+ *
+ * @author jub
+ */
+public interface TrainAttributes {
+
+    public static final String ATTR_GROUP = "group";
+}
