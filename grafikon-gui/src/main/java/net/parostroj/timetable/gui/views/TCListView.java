b46dff50884372be47f5c144019252a15347d65c
jub <jub@parostroj.net>
2013-04-12 14:07:11 +0200

    [gui] icons for list of circulations

--- grafikon-gui/src/main/java/net/parostroj/timetable/gui/views/TCListView.java
+++ grafikon-gui/src/main/java/net/parostroj/timetable/gui/views/TCListView.java
@@ -5,6 +5,8 @@
  */
 package net.parostroj.timetable.gui.views;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.List;
 
 import javax.swing.DefaultListModel;
@@ -12,11 +14,12 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 import net.parostroj.timetable.actions.TrainsCycleSort;
+import net.parostroj.timetable.gui.utils.GuiComponentUtils;
+import net.parostroj.timetable.gui.utils.GuiIcon;
 import net.parostroj.timetable.gui.views.TCDelegate.Action;
 import net.parostroj.timetable.model.Train;
 import net.parostroj.timetable.model.TrainsCycle;
 import net.parostroj.timetable.utils.IdGenerator;
-import net.parostroj.timetable.utils.ResourceLoader;
 
 /**
  * List with all engine cycles and buttons for create and remove.
@@ -24,29 +27,100 @@ import net.parostroj.timetable.utils.ResourceLoader;
  * @author jub
  */
 public class TCListView extends javax.swing.JPanel implements TCDelegate.Listener, ListSelectionListener {
-    
+
     private TCDelegate delegate;
-    
+
     /** Creates new form ECListView */
     public TCListView() {
-        initComponents();
+        setLayout(new BorderLayout(0, 0));
+        ecList = new javax.swing.JList();
+        scrollPane = new javax.swing.JScrollPane();
+        add(scrollPane, BorderLayout.CENTER);
+
+        ecList.setPrototypeCellValue("mmmmmmmmm");
+        ecList.setVisibleRowCount(3);
+        scrollPane.setViewportView(ecList);
         ecList.addListSelectionListener(this);
+
+        javax.swing.JPanel panel = new javax.swing.JPanel();
+        add(panel, BorderLayout.SOUTH);
+        GridBagLayout gbl_panel = new GridBagLayout();
+        panel.setLayout(gbl_panel);
+        newNameTextField = new javax.swing.JTextField();
+        GridBagConstraints gbc_newNameTextField = new GridBagConstraints();
+        gbc_newNameTextField.weightx = 1.0;
+        gbc_newNameTextField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_newNameTextField.insets = new Insets(3, 0, 0, 5);
+        gbc_newNameTextField.gridx = 0;
+        gbc_newNameTextField.gridy = 0;
+        panel.add(newNameTextField, gbc_newNameTextField);
+
+        newNameTextField.setColumns(7);
+        newNameTextField.addCaretListener(new javax.swing.event.CaretListener() {
+            public void caretUpdate(javax.swing.event.CaretEvent evt) {
+                newNameTextFieldCaretUpdate(evt);
+            }
+        });
+        createButton = GuiComponentUtils.createButton(GuiIcon.ADD, 2);
+        GridBagConstraints gbc_createButton = new GridBagConstraints();
+        gbc_createButton.fill = GridBagConstraints.BOTH;
+        gbc_createButton.insets = new Insets(3, 0, 0, 5);
+        gbc_createButton.gridx = 1;
+        gbc_createButton.gridy = 0;
+        panel.add(createButton, gbc_createButton);
+        editButton = GuiComponentUtils.createButton(GuiIcon.EDIT, 2);
+        GridBagConstraints gbc_editButton = new GridBagConstraints();
+        gbc_editButton.fill = GridBagConstraints.BOTH;
+        gbc_editButton.insets = new Insets(3, 0, 0, 5);
+        gbc_editButton.gridx = 2;
+        gbc_editButton.gridy = 0;
+        panel.add(editButton, gbc_editButton);
+        deleteButton = GuiComponentUtils.createButton(GuiIcon.REMOVE, 2);
+        GridBagConstraints gbc_deleteButton = new GridBagConstraints();
+        gbc_deleteButton.insets = new Insets(3, 0, 0, 0);
+        gbc_deleteButton.fill = GridBagConstraints.BOTH;
+        gbc_deleteButton.gridx = 3;
+        gbc_deleteButton.gridy = 0;
+        panel.add(deleteButton, gbc_deleteButton);
+
+        deleteButton.setEnabled(false);
+        deleteButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                deleteButtonActionPerformed(e);
+            }
+        });
+
+        createButton.setEnabled(false);
+        createButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                createButtonActionPerformed(e);
+            }
+        });
+
+        editButton.setEnabled(false);
+        editButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (delegate.getSelectedCycle() != null)
+                    delegate.showEditDialog(editButton);
+            }
+        });
     }
-    
+
     public void setModel(TCDelegate delegate) {
         this.delegate = delegate;
         this.delegate.addListener(this);
 
         this.updateView();
     }
-    
+
     @Override
     public void tcEvent(Action action, TrainsCycle cycle, Train train) {
         switch (action) {
             case REFRESH:
                 this.updateView();
                 break;
-            case NEW_CYCLE: case DELETED_CYCLE:
+            case NEW_CYCLE:
+            case DELETED_CYCLE:
                 this.updateView();
                 break;
             case MODIFIED_CYCLE:
@@ -58,11 +132,12 @@ public class TCListView extends javax.swing.JPanel implements TCDelegate.Listene
                 // nothing
         }
     }
-    
+
     private void updateView() {
         if (delegate.getTrainDiagram() != null && delegate.getType() != null) {
             // set list
-            List<TrainsCycle> sorted = (new TrainsCycleSort(TrainsCycleSort.Type.ASC)).sort(delegate.getTrainDiagram().getCycles(delegate.getType()));
+            List<TrainsCycle> sorted = (new TrainsCycleSort(TrainsCycleSort.Type.ASC)).sort(delegate.getTrainDiagram()
+                    .getCycles(delegate.getType()));
             DefaultListModel listModel = new DefaultListModel();
             for (TrainsCycle cycle : sorted) {
                 listModel.addElement(cycle);
@@ -71,14 +146,14 @@ public class TCListView extends javax.swing.JPanel implements TCDelegate.Listene
         } else {
             ecList.setModel(new DefaultListModel());
         }
-        
+
         this.updateButtonStatus();
     }
-    
+
     private void updateButtonStatus() {
-        boolean status = delegate.getTrainDiagram() != null &&
-            delegate.getType() != null && newNameTextField.getText().length() > 0;
-        
+        boolean status = delegate.getTrainDiagram() != null && delegate.getType() != null
+                && newNameTextField.getText().length() > 0;
+
         createButton.setEnabled(status);
     }
 
@@ -89,84 +164,18 @@ public class TCListView extends javax.swing.JPanel implements TCDelegate.Listene
         // set selected engine
         Object[] selectedValues = ecList.getSelectedValues();
         if (selectedValues.length == 1)
-            delegate.setSelectedCycle((TrainsCycle)ecList.getSelectedValue());
+            delegate.setSelectedCycle((TrainsCycle) ecList.getSelectedValue());
         else
             delegate.setSelectedCycle(null);
         deleteButton.setEnabled(selectedValues.length > 0);
+        editButton.setEnabled(selectedValues.length > 0);
     }
-            
-    /** This method is called from within the constructor to
-     * initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is
-     * always regenerated by the Form Editor.
-     */
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
 
-        scrollPane = new javax.swing.JScrollPane();
-        ecList = new javax.swing.JList();
-        newNameTextField = new javax.swing.JTextField();
-        createButton = new javax.swing.JButton();
-        deleteButton = new javax.swing.JButton();
-
-        scrollPane.setPreferredSize(new java.awt.Dimension(0, 132));
-
-        ecList.setPrototypeCellValue("mmmmmmmmm");
-        ecList.setVisibleRowCount(3);
-        scrollPane.setViewportView(ecList);
-
-        newNameTextField.setColumns(10);
-        newNameTextField.addCaretListener(new javax.swing.event.CaretListener() {
-            public void caretUpdate(javax.swing.event.CaretEvent evt) {
-                newNameTextFieldCaretUpdate(evt);
-            }
-        });
-
-        createButton.setText(ResourceLoader.getString("button.new")); // NOI18N
-        createButton.setEnabled(false);
-        createButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                createButtonActionPerformed(evt);
-            }
-        });
-
-        deleteButton.setText(ResourceLoader.getString("button.delete")); // NOI18N
-        deleteButton.setEnabled(false);
-        deleteButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                deleteButtonActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(createButton, javax.swing.GroupLayout.DEFAULT_SIZE, 72, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(deleteButton, javax.swing.GroupLayout.DEFAULT_SIZE, 82, Short.MAX_VALUE))
-            .addComponent(newNameTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 160, Short.MAX_VALUE)
-            .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 160, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(newNameTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(deleteButton)
-                    .addComponent(createButton)))
-        );
-    }// </editor-fold>//GEN-END:initComponents
-
-    private void deleteButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteButtonActionPerformed
+    private void deleteButtonActionPerformed(java.awt.event.ActionEvent evt) {
         // get selected cycles ...
         Object[] selectedValues = ecList.getSelectedValues();
         for (Object selectedObject : selectedValues) {
-            TrainsCycle cycle = (TrainsCycle)selectedObject;
+            TrainsCycle cycle = (TrainsCycle) selectedObject;
             if (cycle != null) {
                 // remove from diagram
                 delegate.getTrainDiagram().removeCycle(cycle);
@@ -174,14 +183,15 @@ public class TCListView extends javax.swing.JPanel implements TCDelegate.Listene
                 delegate.fireEvent(TCDelegate.Action.DELETED_CYCLE, cycle);
             }
         }
-    }//GEN-LAST:event_deleteButtonActionPerformed
+    }
 
-    private void createButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_createButtonActionPerformed
+    private void createButtonActionPerformed(java.awt.event.ActionEvent evt) {
         // get name from text field (ignore shorter than one character
         if (newNameTextField.getText().length() > 0 && delegate.getType() != null) {
-            TrainsCycle cycle = new TrainsCycle(IdGenerator.getInstance().getId(), newNameTextField.getText(),"", delegate.getTrainDiagram().getCyclesType(delegate.getType()));
+            TrainsCycle cycle = new TrainsCycle(IdGenerator.getInstance().getId(), newNameTextField.getText(), "",
+                    delegate.getTrainDiagram().getCyclesType(delegate.getType()));
             delegate.getTrainDiagram().addCycle(cycle);
-            
+
             // clear field
             newNameTextField.setText("");
             // fire event
@@ -190,17 +200,16 @@ public class TCListView extends javax.swing.JPanel implements TCDelegate.Listene
             ecList.setSelectedValue(cycle, true);
             delegate.fireEvent(TCDelegate.Action.SELECTED_CHANGED, cycle);
         }
-    }//GEN-LAST:event_createButtonActionPerformed
+    }
 
-    private void newNameTextFieldCaretUpdate(javax.swing.event.CaretEvent evt) {//GEN-FIRST:event_newNameTextFieldCaretUpdate
+    private void newNameTextFieldCaretUpdate(javax.swing.event.CaretEvent evt) {
         this.updateButtonStatus();
-    }//GEN-LAST:event_newNameTextFieldCaretUpdate
+    }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton createButton;
-    private javax.swing.JButton deleteButton;
-    private javax.swing.JList ecList;
-    private javax.swing.JTextField newNameTextField;
-    private javax.swing.JScrollPane scrollPane;
-    // End of variables declaration//GEN-END:variables
+    private final javax.swing.JButton createButton;
+    private final javax.swing.JButton deleteButton;
+    private final javax.swing.JButton editButton;
+    private final javax.swing.JList ecList;
+    private final javax.swing.JTextField newNameTextField;
+    private final javax.swing.JScrollPane scrollPane;
 }
