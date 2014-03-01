1f817f91061950e38ce0525dbac803b9c8464b3e
jub <jub@parostroj.net>
2014-03-01 15:21:20 +0100

    [gui] formatting

--- grafikon-gui/src/main/java/net/parostroj/timetable/gui/views/TrainTableColumn.java
+++ grafikon-gui/src/main/java/net/parostroj/timetable/gui/views/TrainTableColumn.java
@@ -26,21 +26,18 @@ import net.parostroj.timetable.utils.ResourceLoader;
  * @author jub
  */
 public enum TrainTableColumn {
-    NODE("train.table.node", 50, 300, 120, "w", String.class, false, null),
-    START("train.table.starttime", 50, 70, 50, "fc", String.class, true, null),
-    END("train.table.endtime", 50, 70, 50, "loc", String.class, true, null),
-    STOP("train.table.stop", 50, 50, 50, "flo", String.class, true, null),
-    REAL_STOP("train.table.real.stop", 50, 50, 50, "w", Double.class, false, null),
-    SPEED("train.table.speed", 50, 50, 50, "e", Integer.class, false, null),
-    ADDED_TIME("train.table.added.time", 50, 50, 50, "e", String.class, true, null),
-    PLATFORM("train.table.platform", 50, 50, 50, "t", String.class, false, new TrackCellEditor()),
-    WEIGHT("train.table.weight", 50, 100, 50, "w", Integer.class, false, null),
-    LENGTH("train.table.length", 50, 100, 50, "w", Integer.class, false, null),
-    CONFLICTS("train.table.conflicts", 50, 200, 75, "w", String.class, false, null),
-    COMMENT_SHOWN("train.table.comment.shown", 30, 30, 30, "o", Boolean.class, false, null),
-    COMMENT("train.table.comment", 1, Integer.MAX_VALUE, 150, "", String.class, false, null),
-    OCCUPIED_ENTRY("train.table.occupied.track", 30, 30, 30, "fo", Boolean.class, false, null),
-    SHUNT("train.table.shunt", 30, 30, 30, "fo", Boolean.class, false, null);
+    NODE("train.table.node", 50, 300, 120, "w", String.class, false, null), START("train.table.starttime", 50, 70, 50,
+            "fc", String.class, true, null), END("train.table.endtime", 50, 70, 50, "loc", String.class, true, null), STOP(
+            "train.table.stop", 50, 50, 50, "flo", String.class, true, null), REAL_STOP("train.table.real.stop", 50,
+            50, 50, "w", Double.class, false, null), SPEED("train.table.speed", 50, 50, 50, "e", Integer.class, false,
+            null), ADDED_TIME("train.table.added.time", 50, 50, 50, "e", String.class, true, null), PLATFORM(
+            "train.table.platform", 50, 50, 50, "t", String.class, false, new TrackCellEditor()), WEIGHT(
+            "train.table.weight", 50, 100, 50, "w", Integer.class, false, null), LENGTH("train.table.length", 50, 100,
+            50, "w", Integer.class, false, null), CONFLICTS("train.table.conflicts", 50, 200, 75, "w", String.class,
+            false, null), COMMENT_SHOWN("train.table.comment.shown", 30, 30, 30, "o", Boolean.class, false, null), COMMENT(
+            "train.table.comment", 1, Integer.MAX_VALUE, 150, "", String.class, false, null), OCCUPIED_ENTRY(
+            "train.table.occupied.track", 30, 30, 30, "fo", Boolean.class, false, null), SHUNT("train.table.shunt", 30,
+            30, 30, "fo", Boolean.class, false, null);
 
     private int index;
     private String key;
@@ -64,69 +61,67 @@ public enum TrainTableColumn {
 
     private static class TimeCellRenderer implements TableCellRenderer {
 
-    	private final static String END;
-    	private TableCellRenderer wrapped;
-		private final int width;
-		private final Icon icon;
+        private final static String END;
+        private TableCellRenderer wrapped;
+        private final int width;
+        private final Icon icon;
 
-    	static {
-    		StringBuilder b = new StringBuilder();
-    		b.append(DecimalFormatSymbols.getInstance().getDecimalSeparator());
-    		b.append('0');
-    		END = b.toString();
-    	}
+        static {
+            StringBuilder b = new StringBuilder();
+            b.append(DecimalFormatSymbols.getInstance().getDecimalSeparator());
+            b.append('0');
+            END = b.toString();
+        }
 
-    	public TimeCellRenderer(TableCellRenderer wrapped) {
-    		this.wrapped = wrapped;
-			JLabel l = (JLabel) wrapped;
-    		this.width = SwingUtilities.computeStringWidth(l.getFontMetrics(l.getFont()), END);
-    		this.icon = new Icon() {
-				@Override
-				public void paintIcon(Component c, Graphics g, int x, int y) {
-					// nothing - empty space
-				}
+        public TimeCellRenderer(TableCellRenderer wrapped) {
+            this.wrapped = wrapped;
+            JLabel l = (JLabel) wrapped;
+            this.width = SwingUtilities.computeStringWidth(l.getFontMetrics(l.getFont()), END);
+            this.icon = new Icon() {
+                @Override
+                public void paintIcon(Component c, Graphics g, int x, int y) {
+                    // nothing - empty space
+                }
 
-				@Override
-				public int getIconWidth() {
-					return width;
-				}
+                @Override
+                public int getIconWidth() {
+                    return width;
+                }
 
-				@Override
-				public int getIconHeight() {
-					return 1;
-				}
-			};
-		}
+                @Override
+                public int getIconHeight() {
+                    return 1;
+                }
+            };
+        }
 
-		@Override
-		public Component getTableCellRendererComponent(JTable table,
-				Object value, boolean isSelected, boolean hasFocus, int row,
-				int column) {
-			String time = (String) value;
-			Component component = null;
-			if (time != null && time.endsWith(END)) {
-				String text = time.substring(0, time.length() - 2);
-				component = wrapped.getTableCellRendererComponent(table, text, isSelected, hasFocus, row, column);
-				final JLabel l = (JLabel) component;
-				l.setIcon(icon);
-				l.setIconTextGap(0);
-				l.setHorizontalTextPosition(JLabel.LEADING);
-				l.setAlignmentX(JLabel.RIGHT);
-			} else {
-				component = wrapped.getTableCellRendererComponent(table, time, isSelected, hasFocus, row, column);
-				JLabel l = (JLabel) component;
-				l.setIcon(null);
-				l.setIconTextGap(0);
-				l.setHorizontalTextPosition(JLabel.LEADING);
-				l.setAlignmentX(JLabel.RIGHT);
-			}
-			return component;
-		}
-
-
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+                boolean hasFocus, int row, int column) {
+            String time = (String) value;
+            Component component = null;
+            if (time != null && time.endsWith(END)) {
+                String text = time.substring(0, time.length() - 2);
+                component = wrapped.getTableCellRendererComponent(table, text, isSelected, hasFocus, row, column);
+                final JLabel l = (JLabel) component;
+                l.setIcon(icon);
+                l.setIconTextGap(0);
+                l.setHorizontalTextPosition(JLabel.LEADING);
+                l.setAlignmentX(JLabel.RIGHT);
+            } else {
+                component = wrapped.getTableCellRendererComponent(table, time, isSelected, hasFocus, row, column);
+                JLabel l = (JLabel) component;
+                l.setIcon(null);
+                l.setIconTextGap(0);
+                l.setHorizontalTextPosition(JLabel.LEADING);
+                l.setAlignmentX(JLabel.RIGHT);
+            }
+            return component;
+        }
     }
 
-    private TrainTableColumn(String key, int minWidth, int maxWidth, int prefWidth, String forbidden, Class<?> clazz, boolean rightAling, TableCellEditor editor) {
+    private TrainTableColumn(String key, int minWidth, int maxWidth, int prefWidth, String forbidden, Class<?> clazz,
+            boolean rightAling, TableCellEditor editor) {
         this.index = Counter.CNT.getAndIncrement();
         this.key = key;
         this.minWidth = minWidth;
@@ -137,7 +132,7 @@ public enum TrainTableColumn {
         this.editor = editor;
         // parse limits
         for (char limit : forbidden.toCharArray()) {
-            switch(limit) {
+            switch (limit) {
                 case 'e':
                     even = true;
                     break;
@@ -157,8 +152,8 @@ public enum TrainTableColumn {
                     oneTrack = true;
                     break;
                 case 'c':
-                	time = true;
-                	break;
+                    time = true;
+                    break;
             }
         }
     }
@@ -205,7 +200,7 @@ public enum TrainTableColumn {
         if (first && row == 0)
             return false;
         if (last && row == max)
-            return  false;
+            return false;
         if (oneTrack) {
             if (interval.getOwner().getTracks().size() == 1)
                 return false;
@@ -222,7 +217,7 @@ public enum TrainTableColumn {
             cellRenderer.setHorizontalAlignment(SwingConstants.RIGHT);
             tableColumn.setCellRenderer(cellRenderer);
             if (time) {
-            	tableColumn.setCellRenderer(new TimeCellRenderer(cellRenderer));
+                tableColumn.setCellRenderer(new TimeCellRenderer(cellRenderer));
             }
         }
         if (this.getEditor() != null)
