3cbe9c95b0414fef3dfb355f8e4c1b4e6cca6de2
jub <jub@parostroj.net>
2014-03-18 15:27:23 +0100

    [model] formatting

--- grafikon-model/src/main/java/net/parostroj/timetable/actions/TrainsCycleSort.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/actions/TrainsCycleSort.java
@@ -1,6 +1,6 @@
 /*
  * NodeSort.java
- * 
+ *
  * Created on 8.9.2007, 14:44:24
  */
 package net.parostroj.timetable.actions;
@@ -11,22 +11,24 @@ import net.parostroj.timetable.model.TrainsCycle;
 
 /**
  * Sorting of nodes.
- * 
+ *
  * @author jub
  */
 public class TrainsCycleSort {
-    
-    public enum Type {ASC, DESC; }
-    
-    private Type type;
+
+    public enum Type {
+        ASC, DESC;
+    }
+
+    private final Type type;
 
     public TrainsCycleSort(Type type) {
         this.type = type;
     }
-    
+
     /**
      * sorts list of engine cycles.
-     * 
+     *
      * @param cycles engine cycles
      * @return sorted list
      */
@@ -34,9 +36,10 @@ public class TrainsCycleSort {
         Comparator<TrainsCycle> comparator = null;
         List<TrainsCycle> newCycles = new ArrayList<TrainsCycle>(cycles);
         switch (type) {
-                                                                                                case ASC:
+            case ASC:
                 comparator = new Comparator<TrainsCycle>() {
-                    private Collator c = Collator.getInstance();
+                    private final Collator c = Collator.getInstance();
+
                     @Override
                     public int compare(TrainsCycle o1, TrainsCycle o2) {
                         return c.compare(o1.getName(), o2.getName());
@@ -45,7 +48,8 @@ public class TrainsCycleSort {
                 break;
             case DESC:
                 comparator = new Comparator<TrainsCycle>() {
-                    private Collator c = Collator.getInstance();
+                    private final Collator c = Collator.getInstance();
+
                     @Override
                     public int compare(TrainsCycle o1, TrainsCycle o2) {
                         return c.compare(o2.getName(), o1.getName());
