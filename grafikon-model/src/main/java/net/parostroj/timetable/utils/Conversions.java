e90cfbed2caa2daa8f2dca55072ec179e6f81aa1
jub <jub@parostroj.net>
2014-03-19 15:38:17 +0100

    [model] method for returning null if the string is empty, trimming is included for non-empty strings

--- grafikon-model/src/main/java/net/parostroj/timetable/utils/Conversions.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/utils/Conversions.java
@@ -4,15 +4,26 @@ import java.awt.Color;
 
 /**
  * Converting utility.
- * 
+ *
  * @author jub
  */
 public class Conversions {
+
     public static Color convertTextToColor(String text) {
         return Color.decode(text);
     }
-    
+
     public static String convertColorToText(Color c) {
         return String.format("0x%02X%02X%02X", c.getRed(), c.getGreen(), c.getBlue());
+    }
+
+    public static String checkAndTrim(String str) {
+        if (str != null) {
+            str = str.trim();
+            if ("".equals(str)) {
+                str = null;
+            }
+        }
+        return str;
     }
 }
