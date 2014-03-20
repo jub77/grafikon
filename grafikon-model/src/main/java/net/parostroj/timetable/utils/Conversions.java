cd283307e01c1c2758bd5ac209c5d0ec7c378220
jub <jub@parostroj.net>
2014-03-20 10:38:09 +0100

    [model] comparison with null in mind

--- grafikon-model/src/main/java/net/parostroj/timetable/utils/Conversions.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/utils/Conversions.java
@@ -26,4 +26,14 @@ public class Conversions {
         }
         return str;
     }
+
+    public static boolean compareWithNull(Object o1, Object o2) {
+        if (o1 == null && o2 == null) {
+            return true;
+        } else if (o1 != null && o1.equals(o2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
