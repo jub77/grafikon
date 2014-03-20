825e69170e946c017d94af95957a6c66db59bf64
jub <jub@parostroj.net>
2014-03-20 14:44:45 +0100

    [gui] removed line classes model event, all diagram changes causes indication of model change

--- grafikon-gui/src/main/java/net/parostroj/timetable/gui/ApplicationModelColleague.java
+++ grafikon-gui/src/main/java/net/parostroj/timetable/gui/ApplicationModelColleague.java
@@ -20,32 +20,9 @@ public class ApplicationModelColleague extends AbstractColleague implements Appl
     @Override
     public void receiveMessage(Object message) {
         if (!model.isModelChanged()) {
-            // react to some events to set changed model
             if (message instanceof TrainDiagramEvent) {
-                TrainDiagramEvent tde = (TrainDiagramEvent) message;
-                if (tde.getType() == GTEventType.TEXT_ITEM_ADDED || tde.getType() == GTEventType.TEXT_ITEM_REMOVED
-                        || tde.getType() == GTEventType.TEXT_ITEM_MOVED || tde.getType() == GTEventType.IMAGE_ADDED
-                        || tde.getType() == GTEventType.IMAGE_REMOVED
-                        || tde.getType() == GTEventType.OUTPUT_TEMPLATE_ADDED
-                        || tde.getType() == GTEventType.OUTPUT_TEMPLATE_MOVED
-                        || tde.getType() == GTEventType.OUTPUT_TEMPLATE_REMOVED
-                        || tde.getType() == GTEventType.GROUP_ADDED || tde.getType() == GTEventType.GROUP_REMOVED
-                        || tde.getType() == GTEventType.CYCLE_TYPE_ADDED
-                        || tde.getType() == GTEventType.CYCLE_TYPE_REMOVED || tde.getType() == GTEventType.ROUTE_ADDED
-                        || tde.getType() == GTEventType.ROUTE_REMOVED || tde.getType() == GTEventType.ATTRIBUTE)
-                    model.setModelChanged(true);
-                if (tde.getType() == GTEventType.NESTED) {
-                    GTEvent<?> nestedEvent = tde.getLastNestedEvent();
-                    if (nestedEvent instanceof TextItemEvent
-                            || nestedEvent instanceof OutputTemplateEvent
-                            || (nestedEvent instanceof TrainEvent && ((TrainEvent) nestedEvent).getType() == GTEventType.ATTRIBUTE))
-                        model.setModelChanged(true);
-                }
-
-            } else if (message instanceof TrainEvent) {
-                TrainEvent te = (TrainEvent) message;
-                if (te.getType() == GTEventType.ATTRIBUTE)
-                    model.setModelChanged(true);
+                // all model changes causes model changed
+                model.setModelChanged(true);
             }
         }
     }
