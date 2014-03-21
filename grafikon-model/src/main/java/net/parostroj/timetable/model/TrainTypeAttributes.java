c9638584431422c9b2e10fdd3f6762f496bc0470
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-03-21 16:24:15 +0100

    [model] attribute names as constants - TrainType

--- grafikon-model/src/main/java/net/parostroj/timetable/model/TrainTypeAttributes.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/TrainTypeAttributes.java
@@ -8,4 +8,10 @@ package net.parostroj.timetable.model;
 public interface TrainTypeAttributes {
 
     public static final String ATTR_SHOW_WEIGHT_INFO = "weight.info";
+    public static final String ATTR_ABBR = "abbr";
+    public static final String ATTR_COLOR = "color";
+    public static final String ATTR_DESC = "desc";
+    public static final String ATTR_PLATFORM = "platform";
+    public static final String ATTR_TRAIN_NAME_TEMPLATE = "trainNameTemplate";
+    public static final String ATTR_TRAIN_COMPLETE_NAME_TEMPLATE = "trainCompleteNameTemplate";
 }
