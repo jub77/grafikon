3e91564c4ec61645c4b2c6d474a09c832803616e
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-03-24 09:57:47 +0100

    [gui] replacing with generic wrapper (trains cycle item)

---
+++ grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/wrappers/TrainsCycleWrapperDelegate.java
@@ -0,0 +1,45 @@
+package net.parostroj.timetable.gui.wrappers;
+
+import net.parostroj.timetable.model.TimeConverter;
+import net.parostroj.timetable.model.TrainsCycleItem;
+
+/**
+ * Delegate for trains cycle items.
+ *
+ * @author jub
+ */
+public class TrainsCycleWrapperDelegate implements WrapperDelegate {
+
+    private final boolean showComment;
+
+    public TrainsCycleWrapperDelegate() {
+        this(true);
+    }
+
+    public TrainsCycleWrapperDelegate(boolean showComment) {
+        this.showComment = showComment;
+    }
+
+    @Override
+    public String toString(Object element) {
+        return toStringItem((TrainsCycleItem) element);
+    }
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        return toStringItem((TrainsCycleItem) o1).compareTo(toStringItem((TrainsCycleItem) o2));
+    }
+
+    public String toStringItem(TrainsCycleItem item) {
+        TimeConverter c = item.getTrain().getTrainDiagram().getTimeConverter();
+        String str = String.format("%s (%s[%s],%s[%s])", item.getTrain().getName(),
+                        item.getFromInterval().getOwnerAsNode().getName(),
+                        c.convertIntToText(item.getStartTime()),
+                        item.getToInterval().getOwnerAsNode().getName(),
+                        c.convertIntToText(item.getEndTime()));
+        if (showComment && item.getComment() != null && !"".equals(item.getComment().trim())) {
+            str = String.format("%s - %s", str, item.getComment());
+        }
+        return str;
+    }
+}
