a212ea0c10446a6f38e50eefc214531ec311a905
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-03-27 23:08:30 +0100

    [model] implementation of type of circulation import

--- grafikon-model/src/main/java/net/parostroj/timetable/model/imports/TrainsCycleTypeImport.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/imports/TrainsCycleTypeImport.java
@@ -1,7 +1,9 @@
 package net.parostroj.timetable.model.imports;
 
-import net.parostroj.timetable.model.ObjectWithId;
-import net.parostroj.timetable.model.TrainDiagram;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.parostroj.timetable.model.*;
 
 /**
  * Import for trains cycle type.
@@ -10,13 +12,37 @@ import net.parostroj.timetable.model.TrainDiagram;
  */
 public class TrainsCycleTypeImport extends Import {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TrainsCycleTypeImport.class);
+
     public TrainsCycleTypeImport(TrainDiagram diagram, TrainDiagram libraryDiagram, ImportMatch match) {
         super(diagram, libraryDiagram, match);
     }
 
     @Override
-    protected ObjectWithId importObjectImpl(ObjectWithId o) {
-        return null;
+    protected ObjectWithId importObjectImpl(ObjectWithId importedObject) {
+        // check class
+        if (!(importedObject instanceof TrainsCycleType)) {
+            // skip other objects
+            return null;
+        }
+        TrainsCycleType importedCycleType = (TrainsCycleType) importedObject;
+
+        // check if cycle type already exist
+        TrainsCycleType checkedCycleType = this.getCycleType(importedCycleType);
+        if (checkedCycleType != null) {
+            String message = "circulation type already exists";
+            this.addError(importedCycleType, message);
+            LOG.debug("{}: {}", message, checkedCycleType);
+            return null;
+        }
+
+        TrainsCycleType type = new TrainsCycleType(this.getId(importedCycleType), importedCycleType.getName(), importedCycleType.getDescription());
+        type.setAttributes(this.importAttributes(importedCycleType.getAttributes()));
+
+        this.getDiagram().addCyclesType(type);
+        this.addImportedObject(type);
+        LOG.trace("Successfully imported circulation type: " + type);
+        return type;
     }
 
 }
