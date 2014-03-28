fb12ddb185bc11a9af7040c1196097862c4d3c79
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-03-28 10:49:33 +0100

    [model] route import finished

--- grafikon-model/src/main/java/net/parostroj/timetable/model/imports/RouteImport.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/imports/RouteImport.java
@@ -1,7 +1,12 @@
 package net.parostroj.timetable.model.imports;
 
-import net.parostroj.timetable.model.ObjectWithId;
-import net.parostroj.timetable.model.TrainDiagram;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.parostroj.timetable.model.*;
 
 /**
  * Route importer.
@@ -10,13 +15,59 @@ import net.parostroj.timetable.model.TrainDiagram;
  */
 public class RouteImport extends Import {
 
+    private static final Logger LOG = LoggerFactory.getLogger(RouteImport.class);
+
     public RouteImport(TrainDiagram diagram, TrainDiagram libraryDiagram, ImportMatch match) {
         super(diagram, libraryDiagram, match);
     }
 
     @Override
-    protected ObjectWithId importObjectImpl(ObjectWithId o) {
-        return null;
+    protected ObjectWithId importObjectImpl(ObjectWithId importedObject) {
+        // check class
+        if (!(importedObject instanceof Route)) {
+            // skip other objects
+            return null;
+        }
+        Route importedRoute = (Route) importedObject;
+
+        // check if the route already exist
+        Route checkedRoute = this.getRoute(importedRoute);
+        if (checkedRoute != null) {
+            String message = "route already exists";
+            this.addError(importedRoute, message);
+            LOG.debug("{}: {}", message, checkedRoute);
+            return null;
+        }
+
+        // create route
+        RouteSegment[] segments = getSegments(importedRoute);
+        if (segments == null) {
+            String message = "nodes missing";
+            this.addError(importedRoute, message);
+            LOG.debug("{}: {}", message, importedRoute);
+            return null;
+        }
+
+        Route route = new Route(this.getId(importedRoute), importedRoute.getName(), segments);
+        route.setNetPart(importedRoute.isNetPart());
+        route.setTrainRoute(importedRoute.isTrainRoute());
+
+        // add to diagram
+        this.getDiagram().addRoute(route);
+        this.addImportedObject(route);
+        LOG.trace("Successfully imported route: " + route);
+        return route;
     }
 
+    public RouteSegment[] getSegments(Route oRoute) {
+        List<RouteSegment> result = new LinkedList<RouteSegment>();
+        for (RouteSegment oSeg : oRoute.getSegments()) {
+            if (oSeg.isLine()) {
+                result.add(this.getLine(oSeg.asLine()));
+            } else if (oSeg.isNode()) {
+                result.add(this.getNode(oSeg.asNode()));
+            }
+        }
+        return result.toArray(new RouteSegment[0]);
+    }
 }
