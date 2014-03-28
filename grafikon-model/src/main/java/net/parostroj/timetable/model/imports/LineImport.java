7b1275fa4dff65b8541f22eff612dae64f1be0f1
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-03-28 08:16:06 +0100

    [model] line import finished

--- grafikon-model/src/main/java/net/parostroj/timetable/model/imports/LineImport.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/imports/LineImport.java
@@ -1,7 +1,9 @@
 package net.parostroj.timetable.model.imports;
 
-import net.parostroj.timetable.model.ObjectWithId;
-import net.parostroj.timetable.model.TrainDiagram;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.parostroj.timetable.model.*;
 
 /**
  * Importing lines.
@@ -10,13 +12,56 @@ import net.parostroj.timetable.model.TrainDiagram;
  */
 public class LineImport extends Import {
 
+    private static final Logger LOG = LoggerFactory.getLogger(LineImport.class);
+
     public LineImport(TrainDiagram diagram, TrainDiagram libraryDiagram, ImportMatch match) {
         super(diagram, libraryDiagram, match);
     }
 
     @Override
-    protected ObjectWithId importObjectImpl(ObjectWithId o) {
-        return null;
+    protected ObjectWithId importObjectImpl(ObjectWithId importedObject) {
+        // check class
+        if (!(importedObject instanceof Line)) {
+            // skip other objects
+            return null;
+        }
+        Line importedLine = (Line) importedObject;
+
+        // check if the train already exist
+        Line checkedLine = this.getLine(importedLine);
+        if (checkedLine != null) {
+            String message = "line already exists";
+            this.addError(importedLine, message);
+            LOG.debug("{}: {}", message, checkedLine);
+            return null;
+        }
+
+        // create line
+        Node iNodeFrom = this.getNode(importedLine.getFrom());
+        Node iNodeTo = this.getNode(importedLine.getTo());
+
+        if (iNodeFrom == null || iNodeTo == null) {
+            String message = "nodes not found";
+            this.addError(importedLine, message);
+            LOG.debug("{}: {}", message, checkedLine);
+            return null;
+        }
+
+        Line line = getDiagram().createLine(this.getId(importedLine), importedLine.getLength(), iNodeFrom, iNodeTo, importedLine.getTopSpeed());
+        line.setAttributes(this.importAttributes(importedLine.getAttributes()));
+
+        // tracks
+        for (LineTrack importedTrack : importedLine.getTracks()) {
+            LineTrack track = new LineTrack(this.getId(importedTrack), importedTrack.getNumber());
+            track.setAttributes(this.importAttributes(importedTrack.getAttributes()));
+            line.addTrack(track);
+        }
+
+        // add to diagram
+        this.getDiagram().getNet().addLine(iNodeFrom, iNodeTo, line);
+        this.addImportedObject(line);
+        LOG.trace("Successfully imported line: " + line);
+        return line;
     }
 
 }
