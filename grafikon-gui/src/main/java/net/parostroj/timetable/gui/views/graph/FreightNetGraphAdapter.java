4d0d3f6787938cb7db13babf8ac85b6a658ae9a1
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-04-01 00:30:04 +0200

    [gui] delete implementation

--- grafikon-gui/src/main/java/net/parostroj/timetable/gui/views/graph/FreightNetGraphAdapter.java
+++ grafikon-gui/src/main/java/net/parostroj/timetable/gui/views/graph/FreightNetGraphAdapter.java
@@ -1,8 +1,5 @@
 package net.parostroj.timetable.gui.views.graph;
 
-import java.util.Collection;
-import java.util.Collections;
-
 import net.parostroj.timetable.actions.TrainComparator;
 import net.parostroj.timetable.gui.wrappers.TrainWrapperDelegate;
 import net.parostroj.timetable.model.FreightNet;
@@ -11,9 +8,6 @@ import org.jgrapht.ListenableGraph;
 
 import com.mxgraph.model.mxCell;
 import com.mxgraph.model.mxGeometry;
-import com.mxgraph.util.mxEvent;
-import com.mxgraph.util.mxEventObject;
-import com.mxgraph.view.mxGraphSelectionModel;
 
 /**
  * Freight net graph adapter.
@@ -39,15 +33,8 @@ public class FreightNetGraphAdapter extends JGraphTAdapter<FreightNet.Node, Frei
         }
     }
 
-    private final SelectionListener connListener;
-
-    public interface SelectionListener {
-        public void selectedConnections(Collection<FreightNet.Connection> connections);
-    }
-
-    public FreightNetGraphAdapter(ListenableGraph<FreightNet.Node, FreightNet.Connection> graph, SelectionListener listener) {
+    public FreightNetGraphAdapter(ListenableGraph<FreightNet.Node, FreightNet.Connection> graph) {
         super(graph);
-        this.connListener = listener;
         this.setConnectableEdges(false);
         this.setAllowDanglingEdges(false);
         this.setEdgeLabelsMovable(false);
@@ -60,19 +47,6 @@ public class FreightNetGraphAdapter extends JGraphTAdapter<FreightNet.Node, Frei
         this.setCellsEditable(false);
         this.setCellsDisconnectable(false);
         this.setDisconnectOnMove(false);
-
-        this.getSelectionModel().addListener(mxEvent.CHANGE, new mxIEventListener() {
-            @Override
-            public void invoke(Object sender, mxEventObject evt) {
-                mxGraphSelectionModel mm = (mxGraphSelectionModel) sender;
-                mxCell cell = (mxCell) mm.getCell();
-                if (cell != null && mm.getCells().length == 1 && cell.getValue() instanceof FreightNet.Connection) {
-                    connListener.selectedConnections(Collections.singletonList((FreightNet.Connection) cell.getValue()));
-                } else {
-                    connListener.selectedConnections(Collections.<FreightNet.Connection>emptyList());
-                }
-            }
-        });
     }
 
     @Override
