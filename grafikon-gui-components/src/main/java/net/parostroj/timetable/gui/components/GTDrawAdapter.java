a1cbc8475411aaf4275bd11a261a30ce55d64893
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-04-03 09:02:27 +0200

    [gui-components] added adapter for GTDraw

---
+++ grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/components/GTDrawAdapter.java
@@ -0,0 +1,66 @@
+package net.parostroj.timetable.gui.components;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import net.parostroj.timetable.model.Node;
+import net.parostroj.timetable.model.Route;
+import net.parostroj.timetable.model.Train;
+
+public abstract class GTDrawAdapter implements GTDraw {
+
+    protected GTDraw draw;
+
+    public GTDrawAdapter(GTDraw draw) {
+        this.draw = draw;
+    }
+
+    public GTDraw getDraw() {
+        return draw;
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        draw.draw(g);
+    }
+
+    @Override
+    public void paintStationNames(Graphics g) {
+        draw.paintStationNames(g);
+    }
+
+    @Override
+    public Route getRoute() {
+        return draw.getRoute();
+    }
+
+    @Override
+    public void setPositionX(int positionX) {
+        draw.setPositionX(positionX);
+    }
+
+    @Override
+    public float getFontSize() {
+        return draw.getFontSize();
+    }
+
+    @Override
+    public void removedTrain(Train train) {
+        draw.removedTrain(train);
+    }
+
+    @Override
+    public void changedTextTrain(Train train) {
+        draw.changedTextTrain(train);
+    }
+
+    @Override
+    public void changedTextNode(Node node) {
+        draw.changedTextNode(node);
+    }
+
+    @Override
+    public void changedTextAllTrains() {
+        draw.changedTextAllTrains();
+    }
+}
