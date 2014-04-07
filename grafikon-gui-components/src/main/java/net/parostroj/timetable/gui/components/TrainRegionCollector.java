977886e3240a0fa3b0c92bcf83c1187ee4c96542
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-04-07 17:15:15 +0200

    [gui-components] removed empty spaces

--- grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/components/TrainRegionCollector.java
+++ grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/components/TrainRegionCollector.java
@@ -14,19 +14,19 @@ import net.parostroj.timetable.utils.Pair;
 
 /**
  * Class for collecting regions of the trains from graphical timetable.
- * 
+ *
  * @author jub
  */
 public class TrainRegionCollector {
-    
-    private Map<Train,List<Pair<Shape, TimeInterval>>> regions;
-    
+
+    private final Map<Train,List<Pair<Shape, TimeInterval>>> regions;
+
     private boolean collected;
-    
+
     private Set<Train> modifiedTrains;
-    
+
     // sensitivity radius
-    private int radius;
+    private final int radius;
 
     public TrainRegionCollector(int radius) {
         regions = new HashMap<Train, List<Pair<Shape, TimeInterval>>>();
@@ -54,24 +54,24 @@ public class TrainRegionCollector {
         this.collected = true;
         modifiedTrains = new HashSet<Train>();
     }
-    
+
     public boolean isCollecting(Train train) {
         return (!collected || modifiedTrains.contains(train));
     }
-    
+
     public void deleteTrain(Train train) {
         regions.remove(train);
     }
-    
+
     public void newTrain(Train train) {
         modifiedTrains.add(train);
     }
-    
+
     public void modifiedTrain(Train train) {
         regions.remove(train);
         modifiedTrains.add(train);
     }
-    
+
     public List<TimeInterval> getTrainForPoint(int x, int y) {
         Rectangle2D cursor = new Rectangle2D.Double(x - radius, y - radius, radius * 2, radius * 2);
         List<TimeInterval> list = new LinkedList<TimeInterval>();
