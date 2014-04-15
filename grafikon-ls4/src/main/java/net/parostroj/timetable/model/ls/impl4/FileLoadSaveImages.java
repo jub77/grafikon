93a326b3512afd08b4591a3144ebabdf022950bd
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-04-15 16:14:15 +0200

    [ls, ls4] using guava for loading/saving images

--- grafikon-ls4/src/main/java/net/parostroj/timetable/model/ls/impl4/FileLoadSaveImages.java
+++ grafikon-ls4/src/main/java/net/parostroj/timetable/model/ls/impl4/FileLoadSaveImages.java
@@ -1,31 +1,34 @@
 package net.parostroj.timetable.model.ls.impl4;
 
 import java.io.*;
-import java.nio.channels.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
+
 import net.parostroj.timetable.model.TimetableImage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.io.*;
+
 /**
  * Class for loading/saving images from/to gtm.
- * 
+ *
  * @author jub
  */
 public class FileLoadSaveImages {
 
     private static final Logger LOG = LoggerFactory.getLogger(FileLoadSaveImages.class.getName());
     private final String location;
-    
+
     public FileLoadSaveImages(String location) {
         this.location = location;
     }
-    
+
     /**
      * saves image for timetable.
-     * 
+     *
      * @param image image
      * @param os zip output stream
      * @throws java.io.IOException
@@ -38,23 +41,16 @@ public class FileLoadSaveImages {
             LOG.warn("Skipping image {} because the gtm doesn't contain a file.", image.getFilename());
             return;
         }
-        FileInputStream is = new FileInputStream(image.getImageFile());
-        try {
-            FileChannel ic = is.getChannel();
-            entry.setSize(ic.size());
-            os.putNextEntry(entry);
-            WritableByteChannel oc = Channels.newChannel(os);
-            ic.transferTo(0, ic.size(), oc);
-        } finally {
-            is.close();
-        }
+
+        ByteSource src = Files.asByteSource(image.getImageFile());
+        entry.setSize(src.size());
+        os.putNextEntry(entry);
+        src.copyTo(os);
     }
 
-    private static long CHUNK = 100000;
-    
     /**
      * loads image for timetable.
-     * 
+     *
      * @param is input stream
      * @param entry current entry
      * @return file
@@ -62,19 +58,10 @@ public class FileLoadSaveImages {
      */
     public File loadTimetableImage(ZipInputStream is, ZipEntry entry) throws IOException {
         File tempFile = File.createTempFile("gt_", ".temp");
-        ReadableByteChannel ic = Channels.newChannel(is);
-        FileOutputStream os = new FileOutputStream(tempFile);
-        FileChannel oc = os.getChannel();
-        try {
-            long position = 0;
-            long read = 0;
-            while ((read = oc.transferFrom(ic, position, CHUNK)) != 0) {
-                position += read;
-            }
-            tempFile.deleteOnExit();
-        } finally {
-            os.close();
-        }
+
+        Files.asByteSink(tempFile).writeFrom(is);
+        tempFile.deleteOnExit();
+
         return tempFile;
     }
 }
