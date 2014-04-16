aeb5e7a0a6dca3404abed5ca5693ed3a0125e32e
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-04-16 14:00:27 +0200

    [output2] using char/byte source to read resource

--- grafikon-output2/src/main/java/net/parostroj/timetable/output2/util/ResourceHelper.java
+++ grafikon-output2/src/main/java/net/parostroj/timetable/output2/util/ResourceHelper.java
@@ -2,8 +2,6 @@ package net.parostroj.timetable.output2.util;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -12,6 +10,10 @@ import net.parostroj.timetable.utils.ResourceBundleUtil;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.ByteSource;
+import com.google.common.io.CharSource;
 
 /**
  * Helper methods.
@@ -22,20 +24,18 @@ public class ResourceHelper {
 
     private static final Logger LOG = LoggerFactory.getLogger(ResourceHelper.class.getName());
 
-    public static String readResource(String filename, ClassLoader cl) {
+    public static String readResource(final String filename, final ClassLoader cl) {
         try {
-            InputStream fis =  (cl != null) ?
-                cl.getResourceAsStream(filename) :
-                ClassLoader.getSystemResourceAsStream(filename);
-            Reader in = new InputStreamReader(fis, "utf-8");
-            char[] buffer = new char[1000];
-            int read;
-            StringBuilder result = new StringBuilder();
-            while ((read = in.read(buffer)) != -1) {
-                result.append(buffer, 0, read);
-            }
-            in.close();
-            return result.toString();
+            ByteSource bs = new ByteSource() {
+                @Override
+                public InputStream openStream() throws IOException {
+                    InputStream fis = (cl != null) ? cl.getResourceAsStream(filename) : ClassLoader
+                            .getSystemResourceAsStream(filename);
+                    return fis;
+                }
+            };
+            CharSource cs = bs.asCharSource(Charsets.UTF_8);
+            return cs.read();
         } catch (IOException e) {
             LOG.warn(e.getMessage(), e);
             return "";
