22deac7a4836221c02d76c8531b6d41249b48a07
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-04-16 13:50:55 +0200

    [model] added comments

--- grafikon-model/src/main/java/net/parostroj/timetable/utils/ResourceBundleUtil.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/utils/ResourceBundleUtil.java
@@ -4,8 +4,22 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.ResourceBundle.Control;
 
+/**
+ * Util for creating resource bundle with default language regardless of system default locale.
+ *
+ * @author jub
+ */
 public class ResourceBundleUtil {
 
+    /**
+     * loads resource bundle.
+     *
+     * @param name name of the resource bundle
+     * @param loader class loader
+     * @param locale requested locale
+     * @param defaultLocale default locale if the requested is not present
+     * @return resource bundle
+     */
     public static ResourceBundle getBundle(String name, ClassLoader loader, Locale locale, Locale defaultLocale) {
         Control control = ResourceBundle.Control.getNoFallbackControl(ResourceBundle.Control.FORMAT_PROPERTIES);
         ResourceBundle bundle = ResourceBundle.getBundle(name, locale, loader, control);
