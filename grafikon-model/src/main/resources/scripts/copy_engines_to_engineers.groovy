9bbe47a4680fad4cd11e346a2fb01665bd8a722f
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-05-19 16:05:20 +0200

    [model] script for copying engine to engineers circulations

---
+++ grafikon-model/src/main/resources/scripts/copy_engines_to_engineers.groovy
@@ -0,0 +1,14 @@
+import net.parostroj.timetable.model.*
+
+def ecs = diagram.getCycles(TrainsCycleType.ENGINE_CYCLE)
+
+for (ec in ecs) {
+    def dc = new TrainsCycle(diagram.createId(), ec.getName(), null, diagram.getCyclesType(TrainsCycleType.DRIVER_CYCLE))
+
+    for (ecItem in ec) {
+        def dcItem = new TrainsCycleItem(dc, ecItem.getTrain(), null, ecItem.getFrom(), ecItem.getTo())
+        dc.addItem(dcItem)
+    }
+
+    diagram.addCycle(dc)
+}
