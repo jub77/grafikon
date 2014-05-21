41e7b3d89fa1265d172db3db8325c5fff0f4d3f4
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-05-21 17:38:23 +0200

    [gui] execution of gui action

---
+++ grafikon-gui-components/src/main/resources/gui_scripts/copy_engines_to_engineers_select.groovy
@@ -0,0 +1,19 @@
+import net.parostroj.timetable.gui.dialogs.ElementSelectionDialog
+import net.parostroj.timetable.model.*
+
+def ecs = diagram.getCycles(TrainsCycleType.ENGINE_CYCLE)
+
+def select = new ElementSelectionDialog(null, true)
+select.setLocationRelativeTo(null)
+ecs = select.selectElements(ecs)
+
+for (ec in ecs) {
+    def dc = new TrainsCycle(diagram.createId(), ec.getName(), null, diagram.getCyclesType(TrainsCycleType.DRIVER_CYCLE))
+
+    for (ecItem in ec) {
+        def dcItem = new TrainsCycleItem(dc, ecItem.getTrain(), null, ecItem.getFrom(), ecItem.getTo())
+        dc.addItem(dcItem)
+    }
+
+    diagram.addCycle(dc)
+}
