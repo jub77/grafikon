491ec65b61499b73207a43f06a05f167bed2148a
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-07-14 13:55:07 +0200

    [model] transition limit implementation added

--- grafikon-model/src/main/java/net/parostroj/timetable/model/FreightDstFilterImpl.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/FreightDstFilterImpl.java
@@ -10,14 +10,19 @@ public class FreightDstFilterImpl extends FreightDstFilter {
     private final FreightDstFilter parent;
     private final List<?> lastNodes;
     private boolean stop;
+    private final Integer transitionLimit;
 
-    protected FreightDstFilterImpl(FreightDstFilter parent, List<?> lastNodes) {
+    protected FreightDstFilterImpl(FreightDstFilter parent, List<?> lastNodes, Integer transitionLimit) {
         this.parent = parent;
         this.lastNodes = lastNodes;
+        this.transitionLimit = transitionLimit;
     }
 
     @Override
     public boolean accepted(FreightDst dst, int level) {
+        if (transitionLimit != null && transitionLimit < level) {
+            return false;
+        }
         if (stop || (parent != null && !parent.accepted(dst, level + 1))) {
             return false;
         }
