9e330b2b70174d5e4ebf9910d507d49de0b0c19a
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-07-14 09:26:51 +0200

    [model] dst filter classes refactored

--- grafikon-model/src/main/java/net/parostroj/timetable/model/FreightDstFilter.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/FreightDstFilter.java
@@ -7,29 +7,16 @@ import java.util.List;
  *
  * @author jub
  */
-public class FreightDstFilter {
+public abstract class FreightDstFilter {
 
-    private final FreightDstFilter parent;
-    private final List<?> lastNodes;
-    private boolean stop;
+    public static FreightDstFilter createFilter() {
+        return new FreightDstFilterEmpty();
+    }
 
     public static FreightDstFilter createFilter(FreightDstFilter current, FNConnection connection) {
         List<?> lastNodes = connection.get(FNConnection.ATTR_LAST_NODES, List.class);
-        return lastNodes == null ? current : new FreightDstFilter(current, lastNodes);
+        return new FreightDstFilterImpl(current, lastNodes);
     }
 
-    protected FreightDstFilter(FreightDstFilter parent, List<?> lastNodes) {
-        this.parent = parent;
-        this.lastNodes = lastNodes;
-    }
-
-    public boolean accepted(FreightDst dst) {
-        if (stop || (parent != null && !parent.accepted(dst))) {
-            return false;
-        }
-        if (lastNodes != null && dst.isNode() && lastNodes.contains(dst.getNode())) {
-            stop = true;
-        }
-        return true;
-    }
+    public abstract boolean accepted(FreightDst dst, int level);
 }
