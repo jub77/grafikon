b3a07766720a1ca8ec00b50c7281eb3a7a1370c4
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-08-01 22:02:23 +0200

    [gui-components,model] fixed scripts (TCI constructor changed)

--- grafikon-model/src/main/resources/scripts/copy_engines_to_engineers.groovy
+++ grafikon-model/src/main/resources/scripts/copy_engines_to_engineers.groovy
@@ -6,7 +6,7 @@ for (ec in ecs) {
     def dc = new TrainsCycle(diagram.createId(), ec.getName(), null, diagram.getCyclesType(TrainsCycleType.DRIVER_CYCLE))
 
     for (ecItem in ec) {
-        def dcItem = new TrainsCycleItem(dc, ecItem.getTrain(), null, ecItem.getFrom(), ecItem.getTo())
+        def dcItem = new TrainsCycleItem(dc, ecItem.getTrain(), null, ecItem.getFrom(), ecItem.getTo(), null)
         dc.addItem(dcItem)
     }
 
