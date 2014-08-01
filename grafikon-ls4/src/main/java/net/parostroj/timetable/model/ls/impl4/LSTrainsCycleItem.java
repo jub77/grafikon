183b332504c40c57cdbd4dc8605b83dc01f0e4fc
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-08-01 21:45:13 +0200

    [ls4] added TCI attributes

--- grafikon-ls4/src/main/java/net/parostroj/timetable/model/ls/impl4/LSTrainsCycleItem.java
+++ grafikon-ls4/src/main/java/net/parostroj/timetable/model/ls/impl4/LSTrainsCycleItem.java
@@ -1,23 +1,26 @@
 package net.parostroj.timetable.model.ls.impl4;
 
 import javax.xml.bind.annotation.XmlType;
+
 import net.parostroj.timetable.model.Train;
 import net.parostroj.timetable.model.TrainDiagram;
 import net.parostroj.timetable.model.TrainsCycle;
 import net.parostroj.timetable.model.TrainsCycleItem;
+import net.parostroj.timetable.model.ls.LSException;
 
 /**
  * Storage for trains cycle item.
  *
  * @author jub
  */
-@XmlType(propOrder = {"train", "comment", "from", "to"})
+@XmlType(propOrder = {"train", "comment", "from", "to", "attributes"})
 public class LSTrainsCycleItem {
 
     private String train;
     private String comment;
     private String from;
     private String to;
+    private LSAttributes attributes;
 
     public LSTrainsCycleItem() {
     }
@@ -31,6 +34,7 @@ public class LSTrainsCycleItem {
         if (item.getTo() != null) {
             this.to = item.getTo().getId();
         }
+        this.attributes = new LSAttributes(item.getAttributes());
     }
 
     public String getComment() {
@@ -65,10 +69,19 @@ public class LSTrainsCycleItem {
         this.train = train;
     }
 
-    public TrainsCycleItem createTrainsCycleItem(TrainsCycle cycle, TrainDiagram diagram) {
+    public LSAttributes getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(LSAttributes attributes) {
+        this.attributes = attributes;
+    }
+
+    public TrainsCycleItem createTrainsCycleItem(TrainsCycle cycle, TrainDiagram diagram) throws LSException {
         Train modelTrain = diagram.getTrainById(train);
         TrainsCycleItem item = new TrainsCycleItem(cycle, modelTrain,
-                comment, modelTrain.getIntervalById(from), modelTrain.getIntervalById(to), null);
+                comment, modelTrain.getIntervalById(from), modelTrain.getIntervalById(to),
+                attributes != null ? attributes.createAttributes(diagram) : null);
         return item;
     }
 }
