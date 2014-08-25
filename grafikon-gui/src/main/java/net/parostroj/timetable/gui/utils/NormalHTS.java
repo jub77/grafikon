05c6b6a20f490b2cf741b9979b124b369dda4321
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-08-25 21:44:54 +0200

    [gui] create empty set instead of set with null

--- grafikon-gui/src/main/java/net/parostroj/timetable/gui/utils/NormalHTS.java
+++ grafikon-gui/src/main/java/net/parostroj/timetable/gui/utils/NormalHTS.java
@@ -6,9 +6,7 @@ import java.util.List;
 import java.util.Set;
 
 import net.parostroj.timetable.gui.*;
-import net.parostroj.timetable.gui.components.GraphicalTimetableView;
-import net.parostroj.timetable.gui.components.HighlightedTrains;
-import net.parostroj.timetable.gui.components.TimeIntervalSelector;
+import net.parostroj.timetable.gui.components.*;
 import net.parostroj.timetable.model.TimeInterval;
 import net.parostroj.timetable.model.Train;
 import net.parostroj.timetable.utils.TransformUtil;
@@ -33,7 +31,8 @@ public class NormalHTS implements HighlightedTrains, TimeIntervalSelector, Appli
             @Override
             public void modelChanged(ApplicationModelEvent event) {
                 if (event.getType() == ApplicationModelEventType.SELECTED_TRAIN_CHANGED) {
-                    set = Collections.singleton(model.getSelectedTrain());
+                    Train selectedTrain = model.getSelectedTrain();
+                    set = selectedTrain == null ? Collections.<Train>emptySet() : Collections.singleton(selectedTrain);
                     view.repaint();
                 }
             }
