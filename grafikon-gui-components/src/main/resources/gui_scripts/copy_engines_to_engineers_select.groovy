5765f1438fc23ef9ae7554e05ef4137844d8af67
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-08-27 09:33:37 +0200

    [gui,model] fixed scripts and added parent component

--- grafikon-gui-components/src/main/resources/gui_scripts/copy_engines_to_engineers_select.groovy
+++ grafikon-gui-components/src/main/resources/gui_scripts/copy_engines_to_engineers_select.groovy
@@ -1,14 +1,14 @@
 import net.parostroj.timetable.gui.dialogs.ElementSelectionDialog
 import net.parostroj.timetable.model.*
 
-def ecs = diagram.getCycles(TrainsCycleType.ENGINE_CYCLE)
+def ecs = diagram.getCycles(diagram.engineCycleType)
 
-def select = new ElementSelectionDialog(null, true)
-select.setLocationRelativeTo(null)
+def select = new ElementSelectionDialog(parent, true)
+select.setLocationRelativeTo(parent)
 ecs = select.selectElements(ecs)
 
 for (ec in ecs) {
-    def dc = new TrainsCycle(diagram.createId(), ec.getName(), null, diagram.getCyclesType(TrainsCycleType.DRIVER_CYCLE))
+    def dc = new TrainsCycle(diagram.createId(), ec.getName(), null, diagram.driverCycleType)
 
     for (ecItem in ec) {
         def dcItem = new TrainsCycleItem(dc, ecItem.getTrain(), null, ecItem.getFrom(), ecItem.getTo(), null)
--- grafikon-model/src/main/resources/scripts/copy_engines_to_engineers.groovy
+++ grafikon-model/src/main/resources/scripts/copy_engines_to_engineers.groovy
@@ -1,9 +1,9 @@
 import net.parostroj.timetable.model.*
 
-def ecs = diagram.getCycles(TrainsCycleType.ENGINE_CYCLE)
+def ecs = diagram.getCycles(diagram.engineCycleType)
 
 for (ec in ecs) {
-    def dc = new TrainsCycle(diagram.createId(), ec.getName(), null, diagram.getCyclesType(TrainsCycleType.DRIVER_CYCLE))
+    def dc = new TrainsCycle(diagram.createId(), ec.getName(), null, diagram.driverCyclesType)
 
     for (ecItem in ec) {
         def dcItem = new TrainsCycleItem(dc, ecItem.getTrain(), null, ecItem.getFrom(), ecItem.getTo(), null)
