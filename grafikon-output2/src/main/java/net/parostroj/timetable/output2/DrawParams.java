cb0beab062b287450fd1eabf9a758b130991f36d
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-10-02 16:07:35 +0200

    [output2] refactoring

--- grafikon-output2/src/main/java/net/parostroj/timetable/output2/DrawParams.java
+++ grafikon-output2/src/main/java/net/parostroj/timetable/output2/DrawParams.java
@@ -4,10 +4,10 @@ package net.parostroj.timetable.output2;
  * @author jub
  */
 public interface DrawParams {
-
+    // GTDraw
     public static final String ROUTES_PARAM = "routes";
     public static final String GT_PARAMS = "gt.params";
-
+    // CirculationDraw
     public static final String CIRCULATIONS_PARAM = "circulations";
     public static final String CD_PARAMS = "cd.params";
 }
