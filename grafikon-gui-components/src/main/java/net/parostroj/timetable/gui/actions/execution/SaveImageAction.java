a38ac175a9661598ee689bc07dc99f6eb0dffbd8
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-10-02 18:17:26 +0200

    [gui-components] using Output for creating output instead of special code (circulation view file output)

--- grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/actions/execution/SaveImageAction.java
+++ grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/actions/execution/SaveImageAction.java
@@ -1,46 +1,34 @@
 package net.parostroj.timetable.gui.actions.execution;
 
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.File;
 
-import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
 import net.parostroj.timetable.gui.dialogs.SaveImageDialog;
 import net.parostroj.timetable.gui.utils.ResourceLoader;
+import net.parostroj.timetable.output2.OutputException;
 
-import org.apache.batik.dom.GenericDOMImplementation;
-import org.apache.batik.svggen.SVGGeneratorContext;
-import org.apache.batik.svggen.SVGGraphics2D;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.w3c.dom.DOMImplementation;
-import org.w3c.dom.Document;
 
 /**
  * Action for saving image.
- * 
+ *
  * @author jub
  */
 public class SaveImageAction extends EventDispatchAfterModelAction {
-    
-    public static interface Image {
-        public void paintImage(Graphics g);
+
+    public static interface DrawOutput {
+        public void draw(Dimension size, File output, SaveImageDialog.Type type) throws OutputException;
     }
-    
+
     private static final Logger log = LoggerFactory.getLogger(SaveImageAction.class);
     private boolean error;
     private final SaveImageDialog dialog;
-    private final Image image;
-    
-    public SaveImageAction(ActionContext context, SaveImageDialog dialog, Image image) {
+    private final DrawOutput image;
+
+    public SaveImageAction(ActionContext context, SaveImageDialog dialog, DrawOutput image) {
         super(context);
         this.dialog = dialog;
         this.image = image;
@@ -52,43 +40,11 @@ public class SaveImageAction extends EventDispatchAfterModelAction {
         setWaitDialogVisible(true);
         long time = System.currentTimeMillis();
         try {
-            Dimension saveSize = dialog.getSaveSize();
-            if (dialog.getImageType() == SaveImageDialog.Type.PNG) {
-                BufferedImage img = new BufferedImage(saveSize.width, saveSize.height, BufferedImage.TYPE_INT_RGB);
-                Graphics2D g2d = img.createGraphics();
-                g2d.setColor(Color.white);
-                g2d.fillRect(0, 0, saveSize.width, saveSize.height);
-                image.paintImage(g2d);
-
-                try {
-                    ImageIO.write(img, "png", dialog.getSaveFile());
-                } catch (IOException e) {
-                    log.warn("Error saving file: " + dialog.getSaveFile(), e);
-                    error = true;
-                }
-            } else if (dialog.getImageType() == SaveImageDialog.Type.SVG) {
-                DOMImplementation domImpl = GenericDOMImplementation.getDOMImplementation();
-
-                // Create an instance of org.w3c.dom.Document.
-                String svgNS = "http://www.w3.org/2000/svg";
-                Document document = domImpl.createDocument(svgNS, "svg", null);
-
-                SVGGeneratorContext context = SVGGeneratorContext.createDefault(document);
-                SVGGraphics2D g2d = new SVGGraphics2D(context, false);
-
-                g2d.setSVGCanvasSize(saveSize);
-
-                image.paintImage(g2d);
-
-                // write to ouput - do not use css style
-                boolean useCSS = false;
-                try {
-                    Writer out = new OutputStreamWriter(new FileOutputStream(dialog.getSaveFile()), "UTF-8");
-                    g2d.stream(out, useCSS);
-                } catch (IOException e) {
-                    log.warn("Error saving file: " + dialog.getSaveFile(), e);
-                    error = true;
-                }
+            try {
+                image.draw(dialog.getSaveSize(), dialog.getSaveFile(), dialog.getImageType());
+            } catch (OutputException e) {
+                log.warn("Error saving file: " + dialog.getSaveFile(), e);
+                error = true;
             }
         } finally {
             log.debug("Image save finished in {}ms", System.currentTimeMillis() - time);
