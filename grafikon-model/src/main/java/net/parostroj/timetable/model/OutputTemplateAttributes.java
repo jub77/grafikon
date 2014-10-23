d69a910eb52ea965bf10f0746c9f7c430b93c062
jakub.cernohorsky <jakub.cernohorsky@localhost>
2014-10-23 08:52:39 +0200

    [model] output attribute

--- grafikon-model/src/main/java/net/parostroj/timetable/model/OutputTemplateAttributes.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/OutputTemplateAttributes.java
@@ -6,6 +6,7 @@ package net.parostroj.timetable.model;
  * @author jub
  */
 public interface OutputTemplateAttributes {
+    public static final String ATTR_OUTPUT = "output";
     public static final String ATTR_OUTPUT_TYPE = "output.type";
     public static final String ATTR_DEFAULT_TEMPLATE = "default.template";
     public static final String ATTR_OUTPUT_EXTENSION = "output.extension";
