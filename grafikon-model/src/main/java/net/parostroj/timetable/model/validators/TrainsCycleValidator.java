cda7984482abdd1c8fd3cc008220bae0847c1326
jub <jub@parostroj.net>
2015-05-09 21:30:53 +0200

    [model] correct engine class if needed...

--- grafikon-model/src/main/java/net/parostroj/timetable/model/validators/TrainsCycleValidator.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/validators/TrainsCycleValidator.java
@@ -2,6 +2,7 @@ package net.parostroj.timetable.model.validators;
 
 import java.util.ArrayList;
 
+import net.parostroj.timetable.model.EngineClass;
 import net.parostroj.timetable.model.TrainsCycle;
 import net.parostroj.timetable.model.TrainsCycleItem;
 import net.parostroj.timetable.model.events.*;
@@ -17,17 +18,28 @@ public class TrainsCycleValidator implements TrainDiagramValidator {
     public boolean validate(GTEvent<?> event) {
         if (event instanceof TrainsCycleEvent && event.getType() == GTEventType.ATTRIBUTE && event.getAttributeChange().checkName(TrainsCycle.ATTR_ENGINE_CLASS)) {
             TrainsCycleEvent tcEvent = (TrainsCycleEvent) event;
-            for (TrainsCycleItem item : new ArrayList<TrainsCycleItem>(tcEvent.getSource().getItems())) {
+            TrainsCycle circulation = tcEvent.getSource();
+            for (TrainsCycleItem item : new ArrayList<TrainsCycleItem>(circulation.getItems())) {
                 item.getTrain().recalculate();
+            }
+            if (circulation.isPartOfSequence()) {
+                // synchronize the same engine class for all circulations in sequence
+                TrainsCycle current = circulation;
+                EngineClass engineClass = circulation.getAttribute(TrainsCycle.ATTR_ENGINE_CLASS, EngineClass.class);
+                while (current.getNext() != circulation) {
+                    current.getAttributes().setRemove(TrainsCycle.ATTR_ENGINE_CLASS, engineClass);
+                    current = current.getNext();
+                }
             }
             return true;
         }
         if (event instanceof TrainDiagramEvent && event.getType() == GTEventType.TRAINS_CYCLE_REMOVED) {
-            TrainsCycle deleted = ((TrainsCycleEvent) event).getSource();
+            TrainsCycle deleted = (TrainsCycle) ((TrainDiagramEvent) event).getObject();
             // handle sequence of circulations
             if (deleted.isPartOfSequence()) {
                 deleted.removeFromSequence();
             }
+            return true;
         }
         return false;
     }
