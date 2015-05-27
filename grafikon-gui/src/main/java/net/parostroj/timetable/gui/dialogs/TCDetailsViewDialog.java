8a61a2e2368cb43ae8175b57f4a146bb00dc5dba
jub <jub@parostroj.net>
2015-05-27 14:16:02 +0200

    [gui] editing company for other circulations

--- grafikon-gui/src/main/java/net/parostroj/timetable/gui/dialogs/TCDetailsViewDialog.java
+++ grafikon-gui/src/main/java/net/parostroj/timetable/gui/dialogs/TCDetailsViewDialog.java
@@ -6,7 +6,10 @@
 package net.parostroj.timetable.gui.dialogs;
 
 import net.parostroj.timetable.gui.views.TCDelegate;
+import net.parostroj.timetable.gui.wrappers.Wrapper;
+import net.parostroj.timetable.gui.wrappers.WrapperListModel;
 import net.parostroj.timetable.model.Attributes;
+import net.parostroj.timetable.model.Company;
 import net.parostroj.timetable.model.TrainsCycle;
 import net.parostroj.timetable.utils.ResourceLoader;
 import net.parostroj.timetable.utils.ObjectsUtil;
@@ -32,27 +35,38 @@ public class TCDetailsViewDialog extends javax.swing.JDialog {
         this.nameTextField.setText(delegate.getSelectedCycle().getName());
         this.descTextField.setText(delegate.getSelectedCycle().getDescription());
         attributesPanel.startEditing(new Attributes(delegate.getSelectedCycle().getAttributes()));
+
+        Company selCompany = delegate.getSelectedCycle().getAttribute(TrainsCycle.ATTR_COMPANY, Company.class);
+        this.companies = new WrapperListModel<Company>(false);
+        Wrapper<Company> emptyCompany = Wrapper.<Company>getEmptyWrapper("-");
+        this.companies.addWrapper(emptyCompany);
+        for (Company company : delegate.getTrainDiagram().getCompanies()) {
+            this.companies.addWrapper(Wrapper.getWrapper(company));
+        }
+        this.companyComboBox.setModel(companies);
+        this.companyComboBox.setSelectedItem(selCompany != null ? Wrapper.getWrapper(selCompany) : emptyCompany);
     }
 
     private void initComponents() {
         javax.swing.JLabel jLabel1 = new javax.swing.JLabel();
         nameTextField = new javax.swing.JTextField();
         javax.swing.JLabel jLabel2 = new javax.swing.JLabel();
+        javax.swing.JLabel companyLabel = new javax.swing.JLabel();
         descTextField = new javax.swing.JTextField();
         attributesPanel = new net.parostroj.timetable.gui.components.AttributesPanel();
         javax.swing.JButton okButton = new javax.swing.JButton();
         javax.swing.JButton cancelButton = new javax.swing.JButton();
+        companyComboBox = new javax.swing.JComboBox<Wrapper<Company>>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         setModal(true);
         setResizable(false);
 
         jLabel1.setText(ResourceLoader.getString("ec.details.name") + ": "); // NOI18N
+        jLabel2.setText(ResourceLoader.getString("ec.details.description") + ": "); // NOI18N
+        companyLabel.setText(ResourceLoader.getString("ec.details.company") + ": "); // NOI18N
 
         nameTextField.setColumns(15);
-
-        jLabel2.setText(ResourceLoader.getString("ec.details.description") + ": "); // NOI18N
-
         descTextField.setColumns(15);
 
         okButton.setText(ResourceLoader.getString("button.ok")); // NOI18N
@@ -85,11 +99,13 @@ public class TCDetailsViewDialog extends javax.swing.JDialog {
                     .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel2)
-                            .addComponent(jLabel1))
+                            .addComponent(jLabel1)
+                            .addComponent(companyLabel))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(nameTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 286, Short.MAX_VALUE)
-                            .addComponent(descTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 286, Short.MAX_VALUE))))
+                            .addComponent(descTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 286, Short.MAX_VALUE)
+                            .addComponent(companyComboBox, javax.swing.GroupLayout.DEFAULT_SIZE, 286, Short.MAX_VALUE))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -103,6 +119,10 @@ public class TCDetailsViewDialog extends javax.swing.JDialog {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(descTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(companyLabel)
+                    .addComponent(companyComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(attributesPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 189, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -124,6 +144,10 @@ public class TCDetailsViewDialog extends javax.swing.JDialog {
         }
         cycle.setDescription(ObjectsUtil.checkAndTrim(descTextField.getText()));
 
+        // write back company
+        Company selCompany = this.companies.getSelectedObject();
+        cycle.getAttributes().setRemove(TrainsCycle.ATTR_COMPANY, selCompany);
+
         // event
         delegate.fireEvent(TCDelegate.Action.MODIFIED_CYCLE, cycle);
 
@@ -134,4 +158,7 @@ public class TCDetailsViewDialog extends javax.swing.JDialog {
     private net.parostroj.timetable.gui.components.AttributesPanel attributesPanel;
     private javax.swing.JTextField descTextField;
     private javax.swing.JTextField nameTextField;
+    private javax.swing.JComboBox<Wrapper<Company>> companyComboBox;
+
+    private WrapperListModel<Company> companies;
 }
