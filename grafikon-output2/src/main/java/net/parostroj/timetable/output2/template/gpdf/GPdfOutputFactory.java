37560baec0d47c96bfc7af9af3401b6dd1ea8a6c
jub <jub@parostroj.net>
2015-06-11 10:59:44 +0200

    [output2] pdf output with groovy using template output

--- grafikon-output2/src/main/java/net/parostroj/timetable/output2/template/gpdf/GPdfOutputFactory.java
+++ grafikon-output2/src/main/java/net/parostroj/timetable/output2/template/gpdf/GPdfOutputFactory.java
@@ -1,13 +1,16 @@
-package net.parostroj.timetable.output2.template.gsp;
+package net.parostroj.timetable.output2.template.gpdf;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
+import javax.xml.transform.TransformerFactory;
+
 import net.parostroj.timetable.output2.Output;
 import net.parostroj.timetable.output2.OutputException;
 import net.parostroj.timetable.output2.OutputFactory;
 import net.parostroj.timetable.output2.groovy.GroovyTemplateFactory;
+import net.parostroj.timetable.output2.pdf.PdfTransformer;
 import net.parostroj.timetable.output2.template.*;
 
 /**
@@ -15,22 +18,22 @@ import net.parostroj.timetable.output2.template.*;
  *
  * @author jub
  */
-public class GspOutputFactory extends OutputFactory {
+public class GPdfOutputFactory extends OutputFactory {
 
-    private static final String TYPE = "groovy";
+    private static final String TYPE = "pdf.groovy";
     private static final List<String> OUTPUT_TYPES;
-    private static final String TEMPLATE_BASE_LOCATION = "templates/groovy/";
+    private static final String TEMPLATE_BASE_LOCATION = "templates/groovy-fo/";
 
     static {
-        OUTPUT_TYPES = Collections.unmodifiableList(Arrays.asList("starts", "ends", "trains",
-                "stations", "train_unit_cycles", "driver_cycles", "engine_cycles",
-                "custom_cycles", "diagram"));
+        OUTPUT_TYPES = Collections.unmodifiableList(Arrays.asList("starts"));
     }
 
     private final GroovyTemplateFactory factory;
+    private final PdfTransformer transformer;
 
-    public GspOutputFactory() {
+    public GPdfOutputFactory() {
         factory = new GroovyTemplateFactory(TEMPLATE_BASE_LOCATION);
+        transformer = new PdfTransformer(TransformerFactory.newInstance());
     }
 
     private Charset getCharset() {
@@ -60,7 +63,10 @@ public class GspOutputFactory extends OutputFactory {
                 throw new OutputException("Unknown type: " + type);
             }
             TemplateWriterFactory templateFactory = () -> factory.getTemplate(type, this.getCharset());
-            return new TemplateOutput(getLocale(), templateFactory);
+            TemplateTransformerFactory transformerFactory = () -> {
+                return (is, os) -> transformer.write(os, is);
+            };
+            return new TemplateOutput(getLocale(), templateFactory, transformerFactory);
         } catch (Exception e) {
             throw new OutputException(e);
         }
