cb0b042bea7e893cbad68d368c59f13316ecf5de
jub <jub@parostroj.net>
2015-06-25 00:58:09 +0200

    [model] removed type of sort

--- grafikon-model/src/main/java/net/parostroj/timetable/actions/ElementSort.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/actions/ElementSort.java
@@ -13,28 +13,14 @@ import com.google.common.collect.Lists;
  */
 public class ElementSort<T> {
 
-    public enum Type {
-        ASC, DESC;
-    }
-
-    private final Type type;
     private final Comparator<? super T> comparator;
     private final Predicate<? super T> filter;
 
     public ElementSort(Comparator<? super T> comparator) {
-        this(Type.ASC, comparator);
-    }
-
-    public ElementSort(Type type, Comparator<? super T> comparator) {
-        this(type, comparator, null);
+        this(comparator, null);
     }
 
     public ElementSort(Comparator<? super T> comparator, Predicate<? super T> filter) {
-        this(Type.ASC, comparator, filter);
-    }
-
-    public ElementSort(Type type, Comparator<? super T> comparator, Predicate<? super T> filter) {
-        this.type = type;
         this.comparator = comparator;
         this.filter = filter;
     }
@@ -48,10 +34,6 @@ public class ElementSort<T> {
     public List<T> sort(Collection<? extends T> elements) {
         List<T> newElements = filter == null ? Lists.newArrayList(elements) :
             Lists.newArrayList(Iterables.filter(elements, filter));
-        Comparator<? super T> lComparator = comparator;
-        if (type == Type.DESC) {
-            lComparator = lComparator.reversed();
-        }
         Collections.sort(newElements, comparator);
         return newElements;
     }
