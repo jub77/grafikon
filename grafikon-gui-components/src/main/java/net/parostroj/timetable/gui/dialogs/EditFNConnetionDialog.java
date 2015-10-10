762c74e0f805b461b99127547d557475aec81c9d
jub <jub@parostroj.net>
2015-10-10 21:58:07 +0200

    [gui-components] editing of new freight net connection attributes

--- grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/dialogs/EditFNConnetionDialog.java
+++ grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/dialogs/EditFNConnetionDialog.java
@@ -1,13 +1,18 @@
 package net.parostroj.timetable.gui.dialogs;
 
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ItemEvent;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JPanel;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.parostroj.timetable.gui.components.ElementSelectionPanel;
 import net.parostroj.timetable.gui.utils.ResourceLoader;
@@ -16,16 +21,6 @@ import net.parostroj.timetable.model.FNConnection;
 import net.parostroj.timetable.model.Node;
 import net.parostroj.timetable.model.TrainDiagram;
 
-import javax.swing.BoxLayout;
-import javax.swing.JCheckBox;
-import javax.swing.border.EtchedBorder;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Edit dialog for freight net connections.
  *
@@ -33,9 +28,33 @@ import org.slf4j.LoggerFactory;
  */
 public class EditFNConnetionDialog extends javax.swing.JDialog {
 
+    enum FilterType {
+
+        TO_NODES(FNConnection.ATTR_TO_NODES, "edit.fnc.to.nodes"),
+        FROM_NODES(FNConnection.ATTR_FROM_NODES, "edit.fnc.from.nodes"),
+        NODES_STOP_INCLUDE(FNConnection.ATTR_LAST_NODES, "edit.fnc.last.nodes"),
+        NODES_STOP_EXCLUDE(FNConnection.ATTR_LAST_NODES_EXCLUDE, "edit.fnc.last.nodes.exclude");
+
+        private String attributeName;
+        private String resourceKey;
+
+        private FilterType(String attributeName, String resourceKey) {
+            this.attributeName = attributeName;
+            this.resourceKey = resourceKey;
+        }
+
+        public String getAttributeName() {
+            return attributeName;
+        }
+
+        public String getResourceKey() {
+            return resourceKey;
+        }
+    }
+
     private static final Logger log = LoggerFactory.getLogger(EditFNConnetionDialog.class);
 
-    private final ElementSelectionPanel<Node> selectionPanel;
+    private final Map<FilterType, ElementSelectionPanel<Node>> selectionPanels;
     private FNConnection connection;
     private final JTextField transLimitTextField;
     private final JCheckBox transLimitCheckBox;
@@ -50,36 +69,27 @@ public class EditFNConnetionDialog extends javax.swing.JDialog {
         panel.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
 
         JButton okButton = new JButton(ResourceLoader.getString("button.ok"));
-        okButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                writeValuesBack();
-                setVisible(false);
-            }
+        okButton.addActionListener(e -> {
+            writeValuesBack();
+            setVisible(false);
         });
         panel.add(okButton);
 
         JButton cancelButton = new JButton(ResourceLoader.getString("button.cancel"));
-        cancelButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                setVisible(false);
-            }
-        });
+        cancelButton.addActionListener(e -> setVisible(false));
         panel.add(cancelButton);
 
         JPanel editPanel = new JPanel();
         getContentPane().add(editPanel, BorderLayout.CENTER);
         editPanel.setLayout(new BoxLayout(editPanel, BoxLayout.Y_AXIS));
 
-        JPanel stopNodesPanel = new JPanel();
-        editPanel.add(stopNodesPanel);
-        stopNodesPanel.setBorder(new TitledBorder(null, ResourceLoader.getString("edit.fnc.last.nodes"),
-                TitledBorder.LEADING, TitledBorder.TOP, null, null));
-        stopNodesPanel.setLayout(new BorderLayout(0, 0));
+        JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+        editPanel.add(tabbedPane);
 
-        selectionPanel = new ElementSelectionPanel<Node>();
-        stopNodesPanel.add(selectionPanel, BorderLayout.CENTER);
+        selectionPanels = new EnumMap<>(FilterType.class);
+        for (FilterType filterType : FilterType.values()) {
+            addSelectionPanel(filterType, tabbedPane);
+        }
 
         JPanel transLimitPanel = new JPanel();
         FlowLayout fl_transLimitPanel = (FlowLayout) transLimitPanel.getLayout();
@@ -90,18 +100,14 @@ public class EditFNConnetionDialog extends javax.swing.JDialog {
 
         transLimitCheckBox = new JCheckBox((String) null);
         transLimitPanel.add(transLimitCheckBox);
-        transLimitCheckBox.addItemListener(new ItemListener() {
-
-            @Override
-            public void itemStateChanged(ItemEvent e) {
-                if (e.getStateChange() == ItemEvent.DESELECTED) {
-                    transLimitTextField.setEnabled(false);
-                    transLimitTextField.setText("");
-                } else {
-                    transLimitTextField.setEnabled(true);
-                    transLimitTextField.setText("0");
-                }
-            }
+        transLimitCheckBox.addItemListener(e -> {
+            if (e.getStateChange() == ItemEvent.DESELECTED) {
+                this.transLimitTextField.setEnabled(false);
+                this.transLimitTextField.setText("");
+            } else {
+                this.transLimitTextField.setEnabled(true);
+                this.transLimitTextField.setText("0");
+            };
         });
 
         JLabel limitLabel = new JLabel(ResourceLoader.getString("edit.fnc.limit") + ": ");
@@ -112,6 +118,13 @@ public class EditFNConnetionDialog extends javax.swing.JDialog {
         transLimitPanel.add(transLimitTextField);
         transLimitTextField.setColumns(5);
         transLimitTextField.setEnabled(false);
+    }
+
+    private void addSelectionPanel(FilterType filterType, JTabbedPane tabbedPane) {
+        ElementSelectionPanel<Node> selectionPanel = new ElementSelectionPanel<Node>();
+        selectionPanels.put(filterType, selectionPanel);
+        selectionPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        tabbedPane.addTab(ResourceLoader.getString(filterType.getResourceKey()), null, selectionPanel, null);
     }
 
     public void edit(Component center, FNConnection connection, TrainDiagram diagram) {
@@ -126,13 +139,8 @@ public class EditFNConnetionDialog extends javax.swing.JDialog {
     }
 
     private void updateValues(TrainDiagram diagram) {
-        // last nodes
-        selectionPanel.setListForSelection(Wrapper.getWrapperList(diagram.getNet().getNodes()));
-        List<Node> lastNodes = connection.getAsList(FNConnection.ATTR_LAST_NODES, Node.class);
-        if (lastNodes != null) {
-            for (Node node : lastNodes) {
-                selectionPanel.addSelected(Wrapper.getWrapper(node));
-            }
+        for (FilterType filterType : FilterType.values()) {
+            updateSelectionPanel(diagram, filterType);
         }
         // transition limit
         Integer transLimit = connection.get(FNConnection.ATTR_TRANSITION_LIMIT, Integer.class);
@@ -140,23 +148,40 @@ public class EditFNConnetionDialog extends javax.swing.JDialog {
         transLimitTextField.setText(transLimit != null ? transLimit.toString() : "");
     }
 
-    private void writeValuesBack() {
-        // last nodes
-        List<Wrapper<Node>> selectedList = selectionPanel.getSelectedList();
-        List<Node> list = Wrapper.unwrap(selectedList);
-        if (list.isEmpty()) {
-            list = null;
+    private void updateSelectionPanel(TrainDiagram diagram, FilterType filterType) {
+        ElementSelectionPanel<Node> selectionPanel = selectionPanels.get(filterType);
+        selectionPanel.setListForSelection(Wrapper.getWrapperList(diagram.getNet().getNodes()));
+        List<Node> lastNodes = connection.getAsList(filterType.getAttributeName(), Node.class);
+        if (lastNodes != null) {
+            for (Node node : lastNodes) {
+                selectionPanel.addSelected(Wrapper.getWrapper(node));
+            }
         }
-        connection.set(FNConnection.ATTR_LAST_NODES, list);
+    }
+
+    private void writeValuesBack() {
+        for (FilterType filterType : FilterType.values()) {
+            writeBackSelectionPanel(filterType);
+        }
         // transition limit
         Integer transLimit = null;
         if (transLimitCheckBox.isSelected()) {
             try {
                 transLimit = Integer.valueOf(transLimitTextField.getText());
             } catch (NumberFormatException e) {
-                log.warn("Error converting: " + e.getMessage());
+                log.warn("Error converting: {}", e.getMessage());
             }
         }
         connection.set(FNConnection.ATTR_TRANSITION_LIMIT, transLimit);
+    }
+
+    private void writeBackSelectionPanel(FilterType filterType) {
+        ElementSelectionPanel<Node> selectionPanel = selectionPanels.get(filterType);
+        List<Wrapper<Node>> selectedList = selectionPanel.getSelectedList();
+        List<Node> list = Wrapper.unwrap(selectedList);
+        if (list.isEmpty()) {
+            list = null;
+        }
+        connection.set(filterType.getAttributeName(), list);
     }
 }
