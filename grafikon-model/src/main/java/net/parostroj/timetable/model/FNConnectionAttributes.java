9c3de5aaf877d86522c17f0bfae3a897b0b66dd9
jub <jub@parostroj.net>
2015-10-10 21:56:53 +0200

    [model] additional freight net connection attributes

--- grafikon-model/src/main/java/net/parostroj/timetable/model/FNConnectionAttributes.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/FNConnectionAttributes.java
@@ -7,5 +7,8 @@ package net.parostroj.timetable.model;
  */
 public interface FNConnectionAttributes {
     public static final String ATTR_LAST_NODES = "last.nodes";
+    public static final String ATTR_LAST_NODES_EXCLUDE = "last.nodes.exclude";
+    public static final String ATTR_FROM_NODES = "from.nodes";
+    public static final String ATTR_TO_NODES = "to.nodes";
     public static final String ATTR_TRANSITION_LIMIT = "transition.limit";
 }
