aa4a6da15ab7983e6106258c6ac7a4b22156e21b
jub <jub@parostroj.net>
2015-10-14 16:18:19 +0200

    [model] added weak listener support to ListenerSupport ;)

--- grafikon-model/src/main/java/net/parostroj/timetable/model/events/ListenerSupport.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/events/ListenerSupport.java
@@ -1,7 +1,6 @@
 package net.parostroj.timetable.model.events;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Listener support. Acts as a delegate for distribution of events.
@@ -11,20 +10,31 @@ import java.util.Set;
 public class ListenerSupport {
 
     private final Set<Listener> listeners;
+    private final Set<Listener> weakListeners;
 
     public ListenerSupport() {
         this.listeners = new HashSet<>();
+        this.weakListeners = Collections.newSetFromMap(new WeakHashMap<>());
     }
 
     public void addListener(Listener listener) {
-        this.listeners.add(listener);
+        if (listener instanceof WeakListener) {
+            this.weakListeners.add(listener);
+        } else {
+            this.listeners.add(listener);
+        }
     }
 
     public void removeListener(Listener listener) {
-        this.listeners.remove(listener);
+        if (listener instanceof WeakListener) {
+            this.weakListeners.remove(listener);
+        } else {
+            this.listeners.remove(listener);
+        }
     }
 
     public void removeAllListeners() {
+        this.weakListeners.clear();
         this.listeners.clear();
     }
 
@@ -32,10 +42,13 @@ public class ListenerSupport {
         for (Listener listener : listeners) {
             listener.changed(event);
         }
+        for (Listener listener : weakListeners) {
+            listener.changed(event);
+        }
     }
 
     @Override
     public String toString() {
-        return String.format("Listener support: %d listeners", listeners.size());
+        return String.format("Listener support: %d listeners", listeners.size() + weakListeners.size());
     }
 }
