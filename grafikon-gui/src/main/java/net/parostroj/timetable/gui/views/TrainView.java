b9f22281a349d7160b2afc401c6b2467930bbfec
jub <jub@parostroj.net>
2016-03-01 10:08:19 +0100

    [gui] skip unknown columns in configuration (from v1.1)

--- grafikon-gui/src/main/java/net/parostroj/timetable/gui/views/TrainView.java
+++ grafikon-gui/src/main/java/net/parostroj/timetable/gui/views/TrainView.java
@@ -334,16 +334,20 @@ public class TrainView extends javax.swing.JPanel implements ApplicationModelLis
             for (String cStr : splitted) {
                 try {
                     String[] ss = cStr.split(",");
-                    TrainTableColumn column = TrainTableColumn.valueOf(ss[0]);
-                    if (column != null) {
-                        TableColumn ac = column.createTableColumn();
-                        if (ss.length > 1) {
-                            int wInt = Integer.parseInt(ss[1]);
-                            if (wInt != 0) {
-                                ac.setPreferredWidth(wInt);
+                    try {
+                        TrainTableColumn column = TrainTableColumn.valueOf(ss[0]);
+                        if (column != null) {
+                            TableColumn ac = column.createTableColumn();
+                            if (ss.length > 1) {
+                                int wInt = Integer.parseInt(ss[1]);
+                                if (wInt != 0) {
+                                    ac.setPreferredWidth(wInt);
+                                }
                             }
+                            shownColumns.add(ac);
                         }
-                        shownColumns.add(ac);
+                    } catch (Exception e) {
+                        log.warn("Error adding column to train view: {}", ss[0]);
                     }
                 } catch (NumberFormatException e) {
                     log.warn("Cannot load columns order for train view: {}", cStr);
