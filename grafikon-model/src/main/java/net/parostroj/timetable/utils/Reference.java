b62aa354063b0a8a3bb86c3e6935e48ddfa87765
jub <jub@parostroj.net>
2016-03-08 09:21:27 +0100

    [model] reference extends supplier (get method)

--- grafikon-model/src/main/java/net/parostroj/timetable/utils/Reference.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/utils/Reference.java
@@ -1,13 +1,13 @@
 package net.parostroj.timetable.utils;
 
+import java.util.function.Supplier;
+
 /**
  * Reference.
  *
  * @author jub
  */
-public interface Reference<T> {
-
-	T get();
+public interface Reference<T> extends Supplier<T> {
 
 	void set(T object);
 }
