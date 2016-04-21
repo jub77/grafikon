718f0bcdde9ca8981d4cea50296759871f75e613
jub <jub@parostroj.net>
2016-04-21 21:47:47 +0200

    [gui] renamed resource bundle for gui

--- grafikon-gui/src/main/java/net/parostroj/timetable/utils/ResourceLoader.java
+++ grafikon-gui/src/main/java/net/parostroj/timetable/utils/ResourceLoader.java
@@ -1,6 +1,6 @@
 /*
  * ResourceLoader.java
- * 
+ *
  * Created on 26.8.2007, 13:57:06
  */
 package net.parostroj.timetable.utils;
@@ -10,20 +10,20 @@ import java.util.ResourceBundle;
 
 /**
  * Wrapper for loading resources.
- * 
+ *
  * @author jub
  */
 public class ResourceLoader {
-    
+
     /**
      * returns localized string for key.
-     * 
+     *
      * @param key key
      * @return localized string
      */
     public static String getString(String key) {
         try {
-            return ResourceBundle.getBundle("gt_texts").getString(key);
+            return ResourceBundle.getBundle("gui").getString(key);
         } catch (MissingResourceException e) {
             // fall back to components resource file
             return net.parostroj.timetable.gui.utils.ResourceLoader.getString(key);
