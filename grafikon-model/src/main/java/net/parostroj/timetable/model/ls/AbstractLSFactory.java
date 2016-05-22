fdb839b9da198e26a745044936d07908bc99966f
jub <jub@parostroj.net>
2016-05-22 22:34:45 +0200

    [model] abstract class for ls factory

---
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/ls/AbstractLSFactory.java
@@ -0,0 +1,45 @@
+package net.parostroj.timetable.model.ls;
+
+import java.io.File;
+import java.util.zip.ZipInputStream;
+
+/**
+ * Abstract LSFactory.
+ *
+ * @author jub
+ */
+class AbstractLSFactory<T extends LSVersions> {
+
+    private final Class<T> cacheType;
+    private final String versionKey;
+    private LSCache<T> lsFileCache;
+
+    public AbstractLSFactory(Class<T> cacheType, String versionKey) {
+        this.versionKey = versionKey;
+        this.cacheType = cacheType;
+    }
+
+    protected void init() {
+        lsFileCache = new LSCache<>(cacheType, versionKey);
+    }
+
+    public T createLatestForSave() throws LSException {
+        return lsFileCache.createLatestForSave();
+    }
+
+    public T createForLoad(ZipInputStream is) throws LSException {
+        return lsFileCache.createForLoad(is);
+    }
+
+    public T createForLoad(File file) throws LSException {
+        return lsFileCache.createForLoad(file);
+    }
+
+    public T createForLoad(ModelVersion modelVersion) throws LSException {
+        return lsFileCache.createForLoad(modelVersion);
+    }
+
+    public T createForLoad(String modelVersion) throws LSException {
+        return this.createForLoad(ModelVersion.parseModelVersion(modelVersion));
+    }
+}
--- grafikon-model/src/main/java/net/parostroj/timetable/model/ls/LSFileFactory.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/ls/LSFileFactory.java
@@ -1,20 +1,19 @@
 package net.parostroj.timetable.model.ls;
 
-import java.io.File;
-import java.util.zip.ZipInputStream;
-
 /**
  * Factory for loading/saving train diagram.
  *
  * @author jub
  */
-public class LSFileFactory {
+public class LSFileFactory extends AbstractLSFactory<LSFile> {
 
     private static final String METADATA_KEY_MODEL_VERSION = "model.version";
     private static final LSFileFactory instance = new LSFileFactory();
     private static boolean initialized = false;
 
-    private LSCache<LSFile> lsFileCache;
+    public LSFileFactory() {
+        super(LSFile.class, METADATA_KEY_MODEL_VERSION);
+    }
 
     public static synchronized LSFileFactory getInstance() {
         if (!initialized) {
@@ -22,29 +21,5 @@ public class LSFileFactory {
             initialized = true;
         }
         return instance;
-    }
-
-    private synchronized void init() {
-        lsFileCache = new LSCache<>(LSFile.class, METADATA_KEY_MODEL_VERSION);
-    }
-
-    public synchronized LSFile createLatestForSave() throws LSException {
-        return lsFileCache.createLatestForSave();
-    }
-
-    public synchronized LSFile createForLoad(ZipInputStream is) throws LSException {
-        return lsFileCache.createForLoad(is);
-    }
-
-    public synchronized LSFile createForLoad(File file) throws LSException {
-        return lsFileCache.createForLoad(file);
-    }
-
-    public synchronized LSFile createForLoad(ModelVersion modelVersion) throws LSException {
-        return lsFileCache.createForLoad(modelVersion);
-    }
-
-    public synchronized LSFile createForLoad(String modelVersion) throws LSException {
-        return this.createForLoad(ModelVersion.parseModelVersion(modelVersion));
     }
 }
