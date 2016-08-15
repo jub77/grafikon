78ec60edafbe50a1be8e08d8ec0d4ca85610e52d
jub <jub@parostroj.net>
2016-08-15 15:53:05 +0200

    [model] fixed script for creating outputs from templates

--- grafikon-model/src/main/resources/scripts/copy_output_templates_to_outputs.groovy
+++ grafikon-model/src/main/resources/scripts/copy_output_templates_to_outputs.groovy
@@ -1,13 +1,10 @@
 import net.parostroj.timetable.model.LocalizedString
 import net.parostroj.timetable.utils.IdGenerator
-import net.parostroj.timetable.utils.ObjectsUtil
 
 for (template in diagram.outputTemplates) {
     def output = diagram.partFactory.createOutput(IdGenerator.instance.id)
-    def description = template.description
-    def translatedDescription = description != null ? description.translate() : template.name
-    String firstLine = ObjectsUtil.getFirstLine(translatedDescription);
-    output.name = LocalizedString.fromString(firstLine)
+    def name = template.name
+    output.name = name == null ? LocalizedString.fromString(template.key) : name 
     output.template = template
     diagram.outputs.add(output)
 }
