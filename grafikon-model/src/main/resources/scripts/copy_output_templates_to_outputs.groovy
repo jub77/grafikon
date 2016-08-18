c8f73ec724614c7a52d9b93fb0814d7f06388b51
jub <jub@parostroj.net>
2016-08-18 17:09:50 +0200

    [model] copy key from template to output

--- grafikon-model/src/main/resources/scripts/copy_output_templates_to_outputs.groovy
+++ grafikon-model/src/main/resources/scripts/copy_output_templates_to_outputs.groovy
@@ -6,5 +6,6 @@ for (template in diagram.outputTemplates) {
     def name = template.name
     output.name = name == null ? LocalizedString.fromString(template.key) : name 
     output.template = template
+    output.key = template.key
     diagram.outputs.add(output)
 }
