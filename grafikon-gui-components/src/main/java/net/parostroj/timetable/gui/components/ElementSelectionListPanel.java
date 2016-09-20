5f8e6796fc643c684009d66ae306b4d15fea655f
jub <jub@parostroj.net>
2016-09-20 09:52:35 +0200

    [gui-components] added simple selection dialog (with list)

---
+++ grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/components/ElementSelectionListPanel.java
@@ -0,0 +1,95 @@
+package net.parostroj.timetable.gui.components;
+
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionListener;
+
+import net.parostroj.timetable.gui.wrappers.Wrapper;
+import net.parostroj.timetable.gui.wrappers.WrapperListModel;
+import javax.swing.JScrollPane;
+
+/**
+ * Panel for elements selection from list.
+ *
+ * @author jub
+ */
+public class ElementSelectionListPanel<T> extends JPanel {
+
+    private static final int VISIBLE_ROW_COUNT = 8;
+
+    private WrapperListModel<T> items;
+
+    private JList<Wrapper<T>> itemList;
+
+    public ElementSelectionListPanel() {
+        this(true);
+    }
+
+    public ElementSelectionListPanel(boolean sorted) {
+        this.items = new WrapperListModel<>(sorted);
+        setLayout(new BorderLayout());
+
+        JScrollPane scrollPane = new JScrollPane();
+        add(scrollPane, BorderLayout.CENTER);
+
+        itemList = new JList<>();
+        itemList.setVisibleRowCount(VISIBLE_ROW_COUNT);
+        itemList.setModel(items);
+        itemList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        scrollPane.setViewportView(itemList);
+
+    }
+
+    public void setMultipleSelection(boolean multiple) {
+        itemList.setSelectionMode(
+                multiple ? ListSelectionModel.MULTIPLE_INTERVAL_SELECTION : ListSelectionModel.SINGLE_SELECTION);
+    }
+
+    public void setListForSelection(Collection<Wrapper<T>> list) {
+        items.clear();
+        items.setListOfWrappers(new ArrayList<>(list));
+    }
+
+    public void setSelected(T selected) {
+        this.setSelected(Collections.singletonList(selected));
+    }
+
+    public void setSelected(Collection<? extends T> selectedCollection) {
+        itemList.clearSelection();
+        for (T selected : selectedCollection) {
+            int index = items.getIndexOfObject(selected);
+            if (index != -1) {
+                itemList.addSelectionInterval(index, index);
+            }
+        }
+    }
+
+    public void addListSelectionListener(ListSelectionListener listener) {
+        itemList.addListSelectionListener(listener);
+    }
+
+    public Collection<T> getSelected() {
+        return itemList.getSelectedValuesList().stream().map(wrapper -> wrapper.getElement())
+                .collect(Collectors.toList());
+    }
+
+    public boolean isSelectionEmpty() {
+        return itemList.isSelectionEmpty();
+    }
+
+    public void setVisibleRows(int visibleRows) {
+        this.itemList.setVisibleRowCount(visibleRows);
+    }
+
+    public void setSorted(boolean sorted) {
+        this.items.setSorted(sorted);
+    }
+}
---
+++ grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/dialogs/ElementSelectionListDialog.java
@@ -0,0 +1,108 @@
+package net.parostroj.timetable.gui.dialogs;
+
+import java.awt.Window;
+import java.util.*;
+
+import net.parostroj.timetable.gui.wrappers.Wrapper;
+import net.parostroj.timetable.gui.utils.ResourceLoader;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+/**
+ * Dialog for selecting elements (from list).
+ *
+ * @author jub
+ */
+public class ElementSelectionListDialog<T> extends javax.swing.JDialog {
+
+    private boolean ok = false;
+
+    public ElementSelectionListDialog(Window parent, boolean modal) {
+        super(parent, modal ? DEFAULT_MODALITY_TYPE : ModalityType.MODELESS);
+        initComponents();
+    }
+
+    public void setSorted(boolean sorted) {
+        elementSelectionPanel.setSorted(sorted);
+    }
+
+    /**
+     * shows dialog and returns list of selected elements.
+     *
+     * @see #selectElements(Collection, Collection)
+     *
+     * @param list list of elements
+     * @return list of selected elements
+     */
+    public Collection<T> selectElements(Collection<? extends T> list) {
+        return this.selectElements(list, null);
+    }
+
+    /**
+     * shows dialog and returns list of selected elements. It returns
+     * <code>null</code> in case cancel was pressed.
+     *
+     * @param list list of element from which the selection is done
+     * @param selected already selected elements
+     * @return list of selected elements
+     */
+    public Collection<T> selectElements(Collection<? extends T> list, Collection<? extends T> selected) {
+        elementSelectionPanel.setListForSelection(this.wrapElements(list));
+        elementSelectionPanel.setSelected(selected == null ? Collections.emptyList() : selected);
+        ok = false;
+        setVisible(true);
+        if (ok) {
+            return elementSelectionPanel.getSelected();
+        } else {
+            return null;
+        }
+    }
+
+    private void initComponents() {
+        elementSelectionPanel = new net.parostroj.timetable.gui.components.ElementSelectionListPanel<>();
+        javax.swing.JButton cancelButton = new javax.swing.JButton();
+        javax.swing.JButton okButton = new javax.swing.JButton();
+
+        elementSelectionPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 0, 5));
+
+        JPanel buttonPanel = new JPanel();
+        FlowLayout flowLayout = (FlowLayout) buttonPanel.getLayout();
+        flowLayout.setAlignment(FlowLayout.RIGHT);
+        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+
+        buttonPanel.add(okButton);
+        buttonPanel.add(cancelButton);
+
+        getContentPane().add(elementSelectionPanel, BorderLayout.CENTER);
+
+        cancelButton.setText(ResourceLoader.getString("button.cancel")); // NOI18N
+        cancelButton.addActionListener(e -> {
+            ok = false;
+            setVisible(false);
+        });
+
+        okButton.setText(ResourceLoader.getString("button.ok")); // NOI18N
+        okButton.addActionListener(e -> {
+            ok = true;
+            setVisible(false);
+        });
+        okButton.setEnabled(false);
+
+        elementSelectionPanel.addListSelectionListener(event -> {
+            if (!event.getValueIsAdjusting()) {
+                okButton.setEnabled(!elementSelectionPanel.isSelectionEmpty());
+            }
+        });
+
+        pack();
+    }
+
+    protected List<Wrapper<T>> wrapElements(Iterable<? extends T> elements) {
+        return Wrapper.getWrapperList(elements);
+    }
+
+    private net.parostroj.timetable.gui.components.ElementSelectionListPanel<T> elementSelectionPanel;
+}
