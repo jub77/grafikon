095b502f380d0cd188f60259b9cb8a7185bed094
jub <jub@parostroj.net>
2017-01-11 11:58:44 +0100

    [gui-components] freight destination panel - selection of node added

--- grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/components/FreightDestinationPanel.java
+++ grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/components/FreightDestinationPanel.java
@@ -1,7 +1,18 @@
 package net.parostroj.timetable.gui.components;
 
-import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ItemEvent;
 
+import javax.swing.BorderFactory;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import net.parostroj.timetable.gui.wrappers.Wrapper;
+import net.parostroj.timetable.gui.wrappers.WrapperListModel;
+import net.parostroj.timetable.model.Node;
 import net.parostroj.timetable.model.TrainDiagram;
 
 /**
@@ -11,6 +22,61 @@ import net.parostroj.timetable.model.TrainDiagram;
  */
 public class FreightDestinationPanel extends JPanel {
 
+    private static final Wrapper<Node> empty = Wrapper.getEmptyWrapper("-");
+
+    private final WrapperListModel<Node> nodesModel;
+    private final JTextArea area;
+
+    public FreightDestinationPanel() {
+        JComboBox<Wrapper<Node>> nodesComboBox = new JComboBox<>();
+        nodesModel = new WrapperListModel<>(true);
+        nodesComboBox.setModel(nodesModel);
+
+        setLayout(new BorderLayout());
+        JPanel topPanel = new JPanel();
+        add(topPanel, BorderLayout.NORTH);
+        FlowLayout topLayout = new FlowLayout();
+        topLayout.setAlignment(FlowLayout.LEFT);
+        topPanel.setLayout(topLayout);
+        topPanel.add(nodesComboBox);
+
+        nodesComboBox.addItemListener(event -> {
+            if (event.getStateChange() == ItemEvent.SELECTED) {
+                Node node = nodesModel.getSelectedObject();
+                updateView(node);
+            }
+        });
+
+        area = new JTextArea();
+        area.setFont(area.getFont().deriveFont(12.0f));
+        area.setLineWrap(true);
+
+        JScrollPane scroll = new JScrollPane(area);
+        scroll.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5), scroll.getBorder()));
+        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        add(scroll, BorderLayout.CENTER);
+    }
+
     public void setDiagram(TrainDiagram diagram) {
+        nodesModel.clear();
+        if (diagram != null) {
+            nodesModel.setListOfWrappers(Wrapper.getWrapperList(diagram.getNet().getNodes()));
+        }
+        nodesModel.addWrapper(empty);
+        nodesModel.setSelectedItem(empty);
+    }
+
+    public void updateView(Node node) {
+        // update selection
+        if (nodesModel.getSelectedObject() != node) {
+            nodesModel.setSelectedObject(node);
+        }
+        StringBuilder text = new StringBuilder();
+        if (node != null) {
+            // process freight
+            text.append(node.getName());
+        }
+        area.setText(text.toString());
     }
 }
