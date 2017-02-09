dd48b2303af96793ee9cc7e50303fc4814fda319
jub <jub@parostroj.net>
2017-02-10 00:12:42 +0100

    [model] filter out trains with freight train units

--- grafikon-model/src/main/resources/scripts/add_managed_freight.groovy
+++ grafikon-model/src/main/resources/scripts/add_managed_freight.groovy
@@ -1,5 +1,5 @@
 for (train in diagram.trains) {
-    if (train.type.category.key == "freight") {
+    if (train.type.category.key == "freight" && !train.cyclesMap.values().any{c -> c.cycle.attributes['freight']}) {
         train.getAttributes().setBool(net.parostroj.timetable.model.Train.ATTR_MANAGED_FREIGHT, true)
     }
 }
