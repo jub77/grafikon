e2563dde0f01803f44d1e03519dd5cfd96269025
jub <jub@parostroj.net>
2017-02-10 00:31:04 +0100

    [gui-components] add trains to managed freight script - selection

---
+++ grafikon-gui-components/src/main/resources/gui_scripts/add_managed_freight_select.groovy
@@ -0,0 +1,14 @@
+import net.parostroj.timetable.gui.dialogs.ElementSelectionDialog
+
+def allTrains = diagram.trains.findAll{train -> !train.attributes['managed.freight'] && train.type.category.key == "freight" && !train.cyclesMap.values().any{c -> c.cycle.attributes['freight']}}
+
+if (!allTrains) return
+
+def select = new ElementSelectionDialog(parent, true)
+select.setLocationRelativeTo(parent)
+def trains = select.selectElements(allTrains)
+
+
+for (train in trains) {
+    train.getAttributes().setBool('managed.freight', true)
+}
