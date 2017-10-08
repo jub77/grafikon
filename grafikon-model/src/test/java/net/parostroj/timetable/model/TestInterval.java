9332fcf1d1d19273e0f153cc4dbb61c81061b771
jub <jub@parostroj.net>
2017-10-08 16:04:10 +0200

    [model] update test

--- grafikon-model/src/test/java/net/parostroj/timetable/model/TestInterval.java
+++ grafikon-model/src/test/java/net/parostroj/timetable/model/TestInterval.java
@@ -1,15 +1,39 @@
 package net.parostroj.timetable.model;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * Basic interval test.
+ *
+ * @author jub
+ */
 public class TestInterval {
     @Test
-    public void testNormalizedInterval() {
+    public void createNormalizedInterval() {
         Interval interval = IntervalFactory.createInterval(3600, 7200);
 
-        assertTrue("normalized", interval.isNormalized());
-        assertEquals("length one hour", 3600, interval.getLength());
+        assertThat("normalized", interval.isNormalized(), is(true));
+        assertThat("length one hour", interval.getLength(), is(3600));
+    }
+
+    @Test
+    public void createNonNormalizedInterval() {
+        Interval interval = IntervalFactory.createInterval(-1800, 1800);
+
+        assertThat("non-normalized", interval.isNormalized(), is(false));
+        assertThat("length one hour", interval.getLength(), is(3600));
+    }
+
+    @Test
+    public void getNormalizedFromNonNormalizedInterval() {
+        Interval nonNormalizedinterval = IntervalFactory.createInterval(-1800, 1800);
+        Interval normalizedInterval = nonNormalizedinterval.normalize();
+
+        assertThat("start is normalized", normalizedInterval.getStart(), is(24 * 3600 - 1800));
+        assertThat("normalized", normalizedInterval.isNormalized(), is(true));
+        assertThat("length one hour", normalizedInterval.getLength(), is(3600));
     }
 }
