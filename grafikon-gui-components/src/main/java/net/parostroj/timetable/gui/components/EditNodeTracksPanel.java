df2db9466481fe08c709203c3d8ffaf96077860c
jub <jub@parostroj.net>
2018-07-03 14:23:32 +0200

    [gui] replace overriding method with listener

--- grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/components/EditNodeTracksPanel.java
+++ grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/components/EditNodeTracksPanel.java
@@ -6,6 +6,8 @@ import java.beans.PropertyChangeListener;
 import javax.swing.BorderFactory;
 
 import org.beanfabrics.Path;
+import org.beanfabrics.event.ModelProviderEvent;
+import org.beanfabrics.event.ModelProviderListener;
 import org.beanfabrics.model.PresentationModel;
 import org.beanfabrics.model.Selection;
 
@@ -21,8 +23,6 @@ public class EditNodeTracksPanel extends BaseEditPanel<NodePM> {
 
     private static final long serialVersionUID = 1L;
 
-    private PropertyChangeListener listener;
-
     public EditNodeTracksPanel() {
         ItemListEditPanel<NodeTrackPM> tracksPanel = new ItemListEditPanel<>(new Path("number"), 5);
         tracksPanel.setModelProvider(localProvider);
@@ -30,25 +30,31 @@ public class EditNodeTracksPanel extends BaseEditPanel<NodePM> {
 
         EditNodeTrackPanel trackPanel = new EditNodeTrackPanel();
         trackPanel.setBorder(BorderFactory.createEmptyBorder(5, 0, 0, 0));
-        listener = evt -> {
+        PropertyChangeListener listener = evt -> {
             Selection<NodeTrackPM> selection = getPresentationModel().getTracks().getSelection();
             trackPanel.setPresentationModel(selection.getIndexes().length == 1 ? selection.getFirst() : null);
         };
 
+        localProvider.addModelProviderListener(new Path("tracks"), new ModelProviderListener() {
+            @Override
+            public void modelLost(ModelProviderEvent evt) {
+                PresentationModel model = evt.getModel();
+                if (model != null) {
+                    model.removePropertyChangeListener("tracks", listener);
+                }
+            }
+
+            @Override
+            public void modelGained(ModelProviderEvent evt) {
+                PresentationModel model = evt.getModel();
+                if (model != null) {
+                    model.addPropertyChangeListener("tracks", listener);
+                }
+            }
+        });
+
         this.setLayout(new BorderLayout());
         this.add(tracksPanel, BorderLayout.CENTER);
         this.add(trackPanel, BorderLayout.SOUTH);
-    }
-
-    @Override
-    public void setPresentationModel(NodePM pModel) {
-        PresentationModel oldModel = super.getPresentationModel();
-        if (oldModel != null) {
-            oldModel.removePropertyChangeListener("tracks", listener);
-        }
-        super.setPresentationModel(pModel);
-        if (pModel != null) {
-            pModel.addPropertyChangeListener("tracks", listener);
-        }
     }
 }
