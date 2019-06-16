cba6dd865aee75de361c32784e6e3395ee6d98b5
jub <jub@parostroj.net>
2019-06-16 20:08:48 +0200

    [gui-components] add transformation for listener support

--- grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/components/ListSelectionSupport.java
+++ grafikon-gui-components/src/main/java/net/parostroj/timetable/gui/components/ListSelectionSupport.java
@@ -2,6 +2,7 @@ package net.parostroj.timetable.gui.components;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 import org.beanfabrics.View;
@@ -28,7 +29,8 @@ public class ListSelectionSupport<T extends PresentationModel> implements ModelP
     private final ListListener listListener;
 
     private final Collection<? extends View<T>> views;
-    private final Supplier<? extends ListPM<T>> listSupplier;
+    private final Supplier<? extends ListPM<?>> listSupplier;
+    private final Supplier<T> selectedSupplier;
 
     public ListSelectionSupport(View<T> view, Supplier<? extends ListPM<T>> listSupplier) {
         this(Collections.singletonList(view), listSupplier);
@@ -37,31 +39,65 @@ public class ListSelectionSupport<T extends PresentationModel> implements ModelP
     public ListSelectionSupport(Collection<? extends View<T>> views, Supplier<? extends ListPM<T>> listSupplier) {
         this.views = views;
         this.listSupplier = listSupplier;
+        this.selectedSupplier = () -> {
+        	ListPM<T> list = listSupplier.get();
+            Selection<T> selection = list.getSelection();
+            T selected = selection.getIndexes().length == 1 ? selection.getFirst() : null;
+            return selected;
+        };
         this.listListener = new ListAdapter() {
             @Override
             public void elementsDeselected(ElementsDeselectedEvent evt) {
-                updateSelected(listSupplier.get());
+                updateSelected(selectedSupplier.get());
             }
 
             @Override
             public void elementsSelected(ElementsSelectedEvent evt) {
-                updateSelected(listSupplier.get());
+                updateSelected(selectedSupplier.get());
             }
         };
     }
 
-    private void updateSelected(ListPM<T> list) {
-        Selection<T> selection = list.getSelection();
-        T selected = selection.getIndexes().length == 1 ? selection.getFirst() : null;
+    public <V extends PresentationModel> ListSelectionSupport(View<T> view, Supplier<? extends ListPM<V>> listSupplier, Function<V, T> getter) {
+    	this(Collections.singletonList(view), listSupplier, getter);
+    }
+
+    public <V extends PresentationModel> ListSelectionSupport(Collection<? extends View<T>> views, Supplier<? extends ListPM<V>> listSupplier, Function<V, T> getter) {
+        this.views = views;
+        this.listSupplier = listSupplier;
+        this.selectedSupplier = () -> {
+        	ListPM<V> list = listSupplier.get();
+            Selection<V> selection = list.getSelection();
+            V selected = selection.getIndexes().length == 1 ? selection.getFirst() : null;
+            return getter.apply(selected);
+        };
+        this.listListener = new ListAdapter() {
+            @Override
+            public void elementsDeselected(ElementsDeselectedEvent evt) {
+                updateSelected(selectedSupplier.get());
+            }
+
+            @Override
+            public void elementsSelected(ElementsSelectedEvent evt) {
+                updateSelected(selectedSupplier.get());
+            }
+        };
+    }
+
+    private void updateSelected(T selected) {
         views.forEach(view -> {
             view.setPresentationModel(selected);
         });
     }
 
+    public ListListener getListListener() {
+		return listListener;
+	}
+
     @Override
     public void modelGained(ModelProviderEvent evt) {
         listSupplier.get().addListListener(listListener);
-        updateSelected(listSupplier.get());
+        updateSelected(selectedSupplier.get());
     }
 
     @Override
