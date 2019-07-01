2fbb54f3ebb792c22dfa72177aed9ab6248324cc
jub <jub@parostroj.net>
2019-07-01 17:01:12 +0200

    [gui] fix location of windows outside of visible area

--- grafikon-gui/src/main/java/net/parostroj/timetable/gui/utils/GuiUtils.java
+++ grafikon-gui/src/main/java/net/parostroj/timetable/gui/utils/GuiUtils.java
@@ -1,10 +1,15 @@
 package net.parostroj.timetable.gui.utils;
 
 import java.awt.Component;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * GUI utils class.
@@ -12,6 +17,10 @@ import java.util.StringTokenizer;
  * @author jub
  */
 public class GuiUtils {
+
+    public static final Logger log = LoggerFactory.getLogger(GuiUtils.class);
+
+    private static final int BORDER_FOR_POSITIONING = 50;
 
     public static void setPosition(String preferences, Component component) {
         if (preferences == null)
@@ -23,12 +32,14 @@ public class GuiUtils {
             values.add(Integer.valueOf(token));
         }
         if (values.size() == 4) {
+            checkAndModifyLocationOnScreen(values);
             Rectangle r = new Rectangle(values.get(0),
                     values.get(1),
                     values.get(2),
                     values.get(3));
             component.setBounds(r);
         } else if (values.size() == 2) {
+            checkAndModifyLocationOnScreen(values);
             Rectangle bounds = component.getBounds();
             bounds.x = values.get(0);
             bounds.y = values.get(1);
@@ -47,4 +58,29 @@ public class GuiUtils {
         return String.format("%d|%d|%d|%d", component.getX(), component.getY(), component.getWidth(), component.getHeight());
     }
 
+    private static void checkAndModifyLocationOnScreen(List<Integer> location) {
+        int x = location.get(0);
+        int y = location.get(1);
+        boolean checked = false;
+        GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice[] devices = environment.getScreenDevices();
+        for (GraphicsDevice device : devices) {
+            Rectangle bounds = device.getDefaultConfiguration().getBounds();
+            Rectangle shrunkBounds = new Rectangle(bounds);
+            // shrink by constant
+            shrunkBounds.width = shrunkBounds.width - BORDER_FOR_POSITIONING;
+            shrunkBounds.height = shrunkBounds.height - BORDER_FOR_POSITIONING;
+            if (shrunkBounds.contains(x, y)) {
+                checked = true;
+                break;
+            }
+        }
+        if (!checked) {
+            // put the location on screen 0
+            Rectangle bounds = devices[0].getDefaultConfiguration().getBounds();
+            location.set(0, bounds.x + BORDER_FOR_POSITIONING);
+            location.set(1, bounds.y + BORDER_FOR_POSITIONING);
+            log.debug("Fixed window location: [{},{}]", location.get(0), location.get(1));
+        }
+    }
 }
