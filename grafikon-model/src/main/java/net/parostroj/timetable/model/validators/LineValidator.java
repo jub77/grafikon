61a3a1d153d6365cd588326324420372477145aa
jub <jub@parostroj.net>
2019-09-06 16:02:58 +0200

    [model] remove deleted line track from connectors

--- grafikon-model/src/main/java/net/parostroj/timetable/model/validators/LineValidator.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/validators/LineValidator.java
@@ -1,9 +1,11 @@
 package net.parostroj.timetable.model.validators;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import net.parostroj.timetable.model.Line;
+import net.parostroj.timetable.model.LineTrack;
 import net.parostroj.timetable.model.TimeInterval;
 import net.parostroj.timetable.model.Train;
 import net.parostroj.timetable.model.events.Event;
@@ -18,17 +20,28 @@ public class LineValidator implements TrainDiagramValidator {
 
     @Override
     public boolean validate(Event event) {
-        if (event.getSource() instanceof Line && event.getType() == Type.ATTRIBUTE && event.getAttributeChange().checkName(Line.ATTR_LENGTH, Line.ATTR_SPEED)) {
+        if (event.getSource() instanceof Line) {
             Line line = (Line) event.getSource();
-            Set<Train> trains = new HashSet<>();
-            for (TimeInterval interval : line) {
-                trains.add(interval.getTrain());
+            if (event.getType() == Type.ATTRIBUTE
+                    && event.getAttributeChange().checkName(Line.ATTR_LENGTH, Line.ATTR_SPEED)) {
+                Set<Train> trains = new HashSet<>();
+                for (TimeInterval interval : line) {
+                    trains.add(interval.getTrain());
+                }
+                // recalculate collected trains
+                for (Train train : trains) {
+                    train.recalculate();
+                }
+                return true;
             }
-            // recalculate collected trains
-            for (Train train : trains) {
-                train.recalculate();
+            if (event.getSource() instanceof Line && event.getType() == Type.REMOVED
+                    && event.getObject() instanceof LineTrack) {
+                LineTrack lineTrack = (LineTrack) event.getObject();
+                line.getFrom().getConnectorForLineTrack(lineTrack)
+                        .ifPresent(c -> c.setLineTrack(Optional.empty()));
+                line.getTo().getConnectorForLineTrack(lineTrack)
+                        .ifPresent(c -> c.setLineTrack(Optional.empty()));
             }
-            return true;
         }
         return false;
     }
