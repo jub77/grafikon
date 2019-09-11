89476ee5975ac4c22bcaab4d92a430d4c0b5543d
jub <jub@parostroj.net>
2019-09-11 16:44:43 +0200

    [gui] stop track selection when focus is lost

--- grafikon-gui/src/main/java/net/parostroj/timetable/gui/views/TrackCellEditor.java
+++ grafikon-gui/src/main/java/net/parostroj/timetable/gui/views/TrackCellEditor.java
@@ -3,6 +3,8 @@ package net.parostroj.timetable.gui.views;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 import java.util.List;
 
 import javax.swing.AbstractCellEditor;
@@ -33,6 +35,14 @@ public class TrackCellEditor extends AbstractCellEditor implements TableCellEdit
         editor.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                if (!ignoreAction) {
+                    fireEditingStopped();
+                }
+            }
+        });
+        editor.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e) {
                 if (!ignoreAction) {
                     fireEditingStopped();
                 }
