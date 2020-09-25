92d7f67dc6efa61d93018addbcda41d2835162d1
jub <jub@parostroj.net>
2020-09-25 17:11:05 +0200

    Remove not need modifiers

--- grafikon-model/src/main/java/net/parostroj/timetable/model/NodeAttributes.java
+++ grafikon-model/src/main/java/net/parostroj/timetable/model/NodeAttributes.java
@@ -7,20 +7,20 @@ package net.parostroj.timetable.model;
  */
 public interface NodeAttributes {
 
-    public static final String IP_NEW_SIGNALS = "new.signals";
+    String IP_NEW_SIGNALS = "new.signals";
 
-    public static final String ATTR_CONTROL_STATION = "control.station";
-    public static final String ATTR_INTERLOCKING_PLANT = "interlocking.plant";
-    public static final String ATTR_TRAPEZOID_SIGN = "trapezoid.sign";
-    public static final String ATTR_LENGTH = "length";
-    public static final String ATTR_NAME = "name";
-    public static final String ATTR_TYPE = "type";
-    public static final String ATTR_ABBR = "abbr";
-    public static final String ATTR_LOCATION = "location";
-    public static final String ATTR_FREIGHT_COLORS = "freight.colors";
-    public static final String ATTR_REGIONS = "regions";
-    public static final String ATTR_CENTER_OF_REGIONS = "center.regions";
-    public static final String ATTR_NOT_STRAIGHT_SPEED = "not.straight.speed";
-    public static final String ATTR_SPEED = "speed";
-    public static final String ATTR_COMPANY = "company";
+    String ATTR_CONTROL_STATION = "control.station";
+    String ATTR_INTERLOCKING_PLANT = "interlocking.plant";
+    String ATTR_TRAPEZOID_SIGN = "trapezoid.sign";
+    String ATTR_LENGTH = "length";
+    String ATTR_NAME = "name";
+    String ATTR_TYPE = "type";
+    String ATTR_ABBR = "abbr";
+    String ATTR_LOCATION = "location";
+    String ATTR_FREIGHT_COLORS = "freight.colors";
+    String ATTR_REGIONS = "regions";
+    String ATTR_CENTER_OF_REGIONS = "center.regions";
+    String ATTR_NOT_STRAIGHT_SPEED = "not.straight.speed";
+    String ATTR_SPEED = "speed";
+    String ATTR_COMPANY = "company";
 }
