44607340cd6f6d31d510e891cefce02f3b6e9a4b
jub <jub@parostroj.net>
2020-09-30 13:39:14 +0200

    [model] copy whole name of the template to output

--- grafikon-model/src/main/resources/scripts/copy_output_templates_to_outputs.groovy
+++ grafikon-model/src/main/resources/scripts/copy_output_templates_to_outputs.groovy
@@ -5,7 +5,7 @@ def outputs = []
 for (template in diagram.outputTemplates) {
     def output = diagram.partFactory.createOutput(IdGenerator.instance.id)
     def name = template.name
-    output.name = name == null ? LocalizedString.fromString(template.key) : LocalizedString.fromString(name.translate())
+    output.name = name == null ? LocalizedString.fromString(template.key) : name
     output.template = template
     output.key = template.key
     outputs.add(output)
