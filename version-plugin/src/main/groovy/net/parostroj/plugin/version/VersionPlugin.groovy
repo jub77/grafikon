b6dae38bc3cb241501603f7af82a5c09e5032f60
jub <jub@parostroj.net>
2021-05-18 14:46:08 +0200

    [version-plugin] add since previous from git describe

--- version-plugin/src/main/groovy/net/parostroj/plugin/version/VersionPlugin.groovy
+++ version-plugin/src/main/groovy/net/parostroj/plugin/version/VersionPlugin.groovy
@@ -13,6 +13,8 @@ import org.gradle.api.*
 class VersionExtension {
 	boolean dirty
 	boolean snapshot
+	int since
+	int sincePrevious
 
 	String buildTimestamp
 	String buildId
@@ -22,6 +24,23 @@ class VersionExtension {
 	String distVersion
 	String baseVersion
 	String shortVersion
+}
+
+@CompileStatic
+class ParsedVersion {
+	String tag
+	int since
+	String hash
+
+	static ParsedVersion parse(String commit, Grgit grgit) {
+		def describe = grgit.describe([longDescr: true, commit: commit] as Map<String, Object>)
+		def match = describe =~ /^(.*)-(\d*)-g([a-f0-9]*)$/
+		if (!match) {
+			return null
+		} else {
+			return new ParsedVersion(tag: match.group(1), since: match.group(2) as int, hash: match.group(3))
+		}
+	}
 }
 
 @CompileStatic
@@ -43,21 +62,29 @@ class VersionPlugin implements Plugin<Project> {
 		ver.buildTimestamp = formatter.format(Instant.now().atOffset(ZoneOffset.UTC))
 		ver.buildId = "${ver.buildTimestamp}-${commitId}"
 
-		def describe = grgit.describe(Collections.singletonMap("longDescr", (Object) Boolean.TRUE))
+		def describe = grgit.describe([longDescr: true] as Map<String, Object>)
 
 		def tagVersion
 		def prerelease
 
-		def match = describe =~ /^(.*)-(\d*)-g([a-f0-9]*)$/
-		if (!match) {
+		def parsedVersion = ParsedVersion.parse('HEAD', grgit)
+
+		if (parsedVersion == null) {
 			tagVersion = Version.valueOf("0.0.0")
 			ver.snapshot = true
 			prerelease = false
 		} else {
-			tagVersion = Version.valueOf(match.group(1))
-			ver.snapshot = match.group(2) != "0"
+			tagVersion = Version.valueOf(parsedVersion.tag)
+			ver.since = parsedVersion.since
+			ver.snapshot = ver.since != 0
 			if ("alfa" == tagVersion.preReleaseVersion) {
 				ver.snapshot = true
+			}
+			if (ver.since == 0) {
+				def previousParsedVersion = ParsedVersion.parse('HEAD~1', grgit)
+				if (previousParsedVersion != null) {
+					ver.sincePrevious = previousParsedVersion.since
+				}
 			}
 			prerelease = tagVersion.preReleaseVersion as Boolean
 		}
@@ -94,6 +121,8 @@ class VersionPlugin implements Plugin<Project> {
 				project.logger.lifecycle("Dist version: {}", scmVersion.distVersion)
 				project.logger.lifecycle("Snapshot: {}", scmVersion.snapshot)
 				project.logger.lifecycle("Dirty: {}", scmVersion.dirty)
+				project.logger.lifecycle("Since: {}", scmVersion.since)
+				project.logger.lifecycle("Since previous: {}", scmVersion.sincePrevious)
 				project.logger.lifecycle("Build timestamp: {}", scmVersion.buildTimestamp)
 				project.logger.lifecycle("Build id: {}", scmVersion.buildId)
 				project.logger.lifecycle("Build hash: {}", scmVersion.buildHash)
