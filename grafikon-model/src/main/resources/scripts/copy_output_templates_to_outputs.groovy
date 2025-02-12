8161d57206ed226593f01ec0753d50b1adab4c4f
jub <jub@parostroj.net>
2025-02-12 23:57:33 +0100

    [model] fix copy outputs script

--- grafikon-model/src/main/resources/scripts/copy_output_templates_to_outputs.groovy
+++ grafikon-model/src/main/resources/scripts/copy_output_templates_to_outputs.groovy
@@ -1,21 +1,21 @@
 import net.parostroj.timetable.model.LocalizedString
 import net.parostroj.timetable.model.ObjectReference
-import net.parostroj.timetable.model.OutputTemplateSimpleStorage
+import net.parostroj.timetable.model.OutputTemplateMapping
 import net.parostroj.timetable.utils.IdGenerator
 
 def outputs = []
 def outputTemplates = diagram.outputTemplates
-def storage = diagram.runtimeInfo.getTemplateMapping
-def fromStorage = false
-if (outputTemplates.empty && storage instanceof OutputTemplateSimpleStorage) {
-    outputTemplates =  storage.templates
-    fromStorage = true
+def mapping = diagram.runtimeInfo.templateMapping
+def fromMapping = false
+if (outputTemplates.empty && mapping instanceof OutputTemplateMapping) {
+    outputTemplates =  mapping.templates
+    fromMapping = true
 }
 for (template in outputTemplates) {
     def output = diagram.partFactory.createOutput(IdGenerator.instance.id)
     def name = template.name
     output.name = name == null ? LocalizedString.fromString(template.key) : name
-    if (fromStorage) {
+    if (fromMapping) {
         output.templateRef = ObjectReference.create(template)
     } else {
         output.template = template
