84a33016889617130cea5453953189a973067289
jub <jub@parostroj.net>
2025-02-24 14:37:45 +0100

    [model] import only embedded output templates

--- grafikon-model/src/main/resources/scripts/copy_output_templates_to_outputs.groovy
+++ grafikon-model/src/main/resources/scripts/copy_output_templates_to_outputs.groovy
@@ -1,26 +1,18 @@
 import net.parostroj.timetable.model.LocalizedString
 import net.parostroj.timetable.model.ObjectReference
-import net.parostroj.timetable.model.OutputTemplateMapping
 import net.parostroj.timetable.utils.IdGenerator
 
 def outputs = []
-def outputTemplates = diagram.outputTemplates
-def mapping = diagram.runtimeInfo.templateMapping
-def fromMapping = false
-if (outputTemplates.empty && mapping instanceof OutputTemplateMapping) {
-    outputTemplates =  mapping.templates
-    fromMapping = true
-}
-for (template in outputTemplates) {
-    def output = diagram.partFactory.createOutput(IdGenerator.instance.id)
-    def name = template.name
-    output.name = name == null ? LocalizedString.fromString(template.key) : name
-    if (fromMapping) {
+def storage = diagram.runtimeInfo.templateStorage
+if (storage != null) {
+    def outputTemplates = storage.templates
+    for (template in outputTemplates) {
+        def output = diagram.partFactory.createOutput(IdGenerator.instance.id)
+        def name = template.name
+        output.name = name == null ? LocalizedString.fromString(template.key) : name
         output.templateRef = ObjectReference.create(template)
-    } else {
-        output.template = template
+        output.key = template.key
+        outputs.add(output)
     }
-    output.key = template.key
-    outputs.add(output)
+    diagram.outputs.addAll(outputs)
 }
-diagram.outputs.addAll(outputs)
