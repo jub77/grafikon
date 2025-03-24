1ed8dc49301b3380a191c9b9a347bb78f600ed08
jub <jub@parostroj.net>
2025-03-24 15:33:06 +0100

    [templates] pass features as parameter

--- grafikon-templates/src/main/java/net/parostroj/timetable/model/templates/TemplateLoader.java
+++ grafikon-templates/src/main/java/net/parostroj/timetable/model/templates/TemplateLoader.java
@@ -3,16 +3,17 @@ package net.parostroj.timetable.model.templates;
 import net.parostroj.timetable.loader.DataItemLoader;
 import net.parostroj.timetable.loader.LoadDelegate;
 import net.parostroj.timetable.model.TrainDiagram;
+import net.parostroj.timetable.model.ls.LSFeature;
 
 import java.net.URL;
 
 public interface TemplateLoader {
 
-    static DataItemLoader<TrainDiagram> getDefault() {
-        return DataItemLoader.getFromResources("/templates", "list.yaml", LoadDelegate.createForTrainDiagram());
+    static DataItemLoader<TrainDiagram> getDefault(LSFeature... features) {
+        return DataItemLoader.getFromResources("/templates", "list.yaml", LoadDelegate.createForTrainDiagram(features));
     }
 
-    static DataItemLoader<TrainDiagram> getDefaultFromUrl(URL url) {
-        return DataItemLoader.getFromUrl(url, "list.yaml", LoadDelegate.createForTrainDiagram());
+    static DataItemLoader<TrainDiagram> getDefaultFromUrl(URL url, LSFeature... features) {
+        return DataItemLoader.getFromUrl(url, "list.yaml", LoadDelegate.createForTrainDiagram(features));
     }
 }
